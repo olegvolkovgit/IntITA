5f30c106bdcfde59099795b2ba695c109208a1ec
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-04-24 15:55:49 +0300

    JS parse time consultation and show-hide block calendar on click

---
+++ scripts/parseTable.js
@@ -0,0 +1,41 @@
+/* функция обработки таблицы */
+function parseTable($mytable) {
+    var arr = [];
+    var tbl = document.getElementById($mytable);
+    for (var r=0; r<tbl.rows.length; r++) {
+        // последовательнный перебор всех рядов в каждой таблице
+        for (var c=0; c<tbl.rows[r].cells.length; c++) {
+            // последовательный перебор всех ячеек
+            var cls = tbl.rows[r].cells[c];
+            if(cls.className=='pressedTime'){
+                arr.push(cls.innerHTML)
+            }
+        }
+    }
+    return intervalConsultationArr(arr); //преобразуем в интервалы
+}
+/* 1)Заносим соседнее время в ячейки массива 2)Соеденяем соседнии интервалы в один*/
+function intervalConsultationArr($allTime) {
+/* 1)*/
+    var arr = $allTime;
+    var arrT = [];
+    var i=0;
+    for (var t=0; t<arr.length; t++) {
+        if(arr.length==1) arrT[i]=arr[t];
+        if(arr[t+1] && (arr[t].substr(6,5)==arr[t+1].substr(0,5))){
+            if(!arrT[i]) arrT[i]=arr[t];
+            arrT[i]=arrT[i]+arr[t+1];
+        }
+        if((arr[t+1] && (arr[t].substr(6,5)!==arr[t+1].substr(0,5))) || !arr[t+1]){
+            if(!arrT[i]) arrT[i]=arr[t];
+            arrT[i]=arrT[i];
+            i++;
+        }
+    }
+/* 2)*/
+    var arrI = [];
+    for (var j=0; j<arrT.length; j++) {
+        arrI[j]=' з '+arrT[j].substr(0,5)+' по '+arrT[j].substr(arrT[j].lastIndexOf('-')+1,6);
+    }
+    return arrI;
+}
---
+++ scripts/showHideCalendarTabs.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Wizlight on 24.04.2015.
+ */
+/*при клику на ссылку открывается календарь*/
+$('#consultationCalendar').click(function() {
+    $('#dateTimePicker').focus();
+});
+/*при выборе даты открывается блок с выбором время*/
+$("#dateTimePicker").change(function (){
+    document.getElementById('timeDate').innerHTML= document.getElementById('dateTimePicker').value;
+    $("#timeConsultation").css('display', 'block');
+});
+/*при нажатии на кнопку назад - открывается календарь*/
+$("#consultationBack").click(function (){
+    $("#timeConsultation").css('display', 'none');
+    $('#dateTimePicker').focus();
+});
+/*при нажатии на кнопку даты в окне время - открывается календарь*/
+$("#timeDate").click(function (){
+    $("#timeConsultation").css('display', 'none');
+    $('#dateTimePicker').focus();
+});
+/*при нажатии на кнопку далее - блок время закрывается, открывается информативное окно c переданными ему интервалами консультаций*/
+$("#consultationNext").click(function (){
+    if(parseTable('timeGrid').length!== 0){
+        $("#timeConsultation").css('display', 'none');
+        var textinfo = document.getElementById('consInfText');
+        var dateinfo = document.getElementById('dateTimePicker').value +' року'+ parseTable('timeGrid');
+        textinfo.innerHTML = dateinfo + textinfo.innerHTML;
+        $("#consultationInfo").css('display', 'block');
+    }
+});
+/*при клику на кнопку информационного окна о консультации оно закрывается*/
+$('#consultationButton').click(function() {
+    document.getElementById('consultationInfo').style.display="none";
+});
+/*закрывает окно при клику вне окна*/
+$(document).mouseup(function (e) {
+    var container = $("#timeConsultation");
+    if (container.has(e.target).length === 0){
+        container.hide();
+    }
+});
