952c74c8a131ccfd62747237ac7020c6b68641be
Olha Shenyavska <olha11@ua.fm>
2015-04-26 20:27:34 +0300

    footer

--- protected/views/lesson/index.php
+++ protected/views/lesson/index.php
@@ -36,28 +36,97 @@ $this->breadcrumbs=array(
 
 <div class="lessonBlock" id="lessonBlock">
     <?php $this->renderPartial('_sidebar', array('lecture'=>$lecture, 'skype'=>$lecture->getTeacherInfoById(1)['skype']));?>
-<div class="lessonText">
-    <h1 class="lessonTheme">Змінні та типи даних в PHP </h1>
-    <span class="listTheme">Зміст </span><span class="spoilerLinks"><span class="spoilerClick">(показати)</span><span class="spoilerTriangle"> &#9660;</span></span>
+    <div class="lessonText">
 
-    <div class="spoilerBody">
-        <p><a href="#Частина 1: Типи змінних та перемінних">Частина 1: Типи змінних та перемінних</a></p>
-        <p><a href="#Частина 7: Типи данних та математичний аналіз">Частина 7: Типи данних та математичний аналіз</a></p>
-    </div>
-<!-- Lesson content-->
-<?php
 
-$this->widget('zii.widgets.CListView', array(
-    'dataProvider'=>$dataProvider,
-    'itemView'=>'_content',
-    'summaryText' => '',
-    'emptyText' => 'В данной лекции еще ничего нет (',
-));
-?>
+        <!--start new block imperavi redactor-->
+        <div class="imperaviSimple">
+            <!--start toolbar for wysiwyg-->
+            <div class="btns-imperaviSimple" style="width: 100%; height: 30px; border: solid 0px black; margin-bottom: 10px;">
+                <div class="wrapper-imperaviSemple" style="width: 85%; height: 100%; border: solid 0px black; display: inline-block; float: left;"></div>
+                <div class="btn-edit-ImperaviSimple"
+                     style="width: 5%; height: 100%; border: solid 0px black; display: inline-block; float: right; text-align: center; padding-top: 3px; cursor: pointer;"
+                     onclick="pressEditRedactor('.redactor');" <?$field = '.redactor'?>
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/icons/edt_30px.png" width="26px" height="20px">
+                </div>
+                <div class="btn-cancel-ImperaviSimple"
+                     style="width: 5%; height: 100%; border: solid 0px black; float: right; text-align: center; padding-top: 3px; cursor: pointer; display: none;"
+                     onclick="pressCancelRedactor('.redactor')">
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/icons/cls_30px.png" width="20px" height="20px">
+                </div>
+                <div class="btn-save-ImperaviSimple"
+                     style="width:5%; height: 100%; border: solid 0px black; float: right; text-align: center; padding-top: 3px; cursor: pointer; display: none;"
+                     onclick="pressSaveRedactor('.redactor');">
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/icons/sv_30px.png" width="20px" height="20px">
+                </div>
+            </div>
+            <!--end toolbar for wysiwyg-->
+
+            <script type="text/javascript">
+                function pressEditRedactor(className)
+                {
+                    var selector = className;
+                    $(selector).redactor({
+                        focus: true
+                    });
+                    $('.btn-edit-ImperaviSimple').hide();
+                    $('.btn-save-ImperaviSimple').show();
+                    $('.btn-cancel-ImperaviSimple').show();
+                }
+
+                function pressCancelRedactor(className)
+                {
+                    var selector = className;
+                    $(selector).redactor('core.destroy');
+                    $('.btn-edit-ImperaviSimple').show();
+                    $('.btn-save-ImperaviSimple').hide();
+                    $('.btn-cancel-ImperaviSimple').hide();
+                }
+
+                function pressSaveRedactor(className)
+                {
+                    var selector = className;
+                    // save content if you need
+                    var text = $(selector).redactor('code.get');
+
+                    // destroy editor
+                    $(selector).redactor('core.destroy');
+                    $('.btn-edit-ImperaviSimple').show();
+                    $('.btn-save-ImperaviSimple').hide();
+                    $('.btn-cancel-ImperaviSimple').hide();
+                }
+            </script>
+
+            <!--start include wysiwyg-->
+
+            <div class="redactor">
+
+            </div>
+            <!--end include wysiwyg-->
+        </div>
+        <!--end new block imperavi redactor-->
+	
+        <h1 class="lessonTheme">Змінні та типи даних в PHP </h1>
+        <span class="listTheme">Зміст </span><span class="spoilerLinks"><span class="spoilerClick">(показати)</span><span class="spoilerTriangle"> &#9660;</span></span>
+
+        <div class="spoilerBody">
+            <p><a href="#Частина 1: Типи змінних та перемінних">Частина 1: Типи змінних та перемінних</a></p>
+            <p><a href="#Частина 7: Типи данних та математичний аналіз">Частина 7: Типи данних та математичний аналіз</a></p>
+        </div>
+        <!-- Lesson content-->
+        <?php
+
+        $this->widget('zii.widgets.CListView', array(
+            'dataProvider'=>$dataProvider,
+            'itemView'=>'_content',
+            'summaryText' => '',
+            'emptyText' => 'В данной лекции еще ничего нет (',
+        ));
+        ?>
 <table ><tr><td>
         <div class="download" id="do4">  <a  href="#"><img style="" src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/000zav-yrok.png">Завантажити урок</a></div>
             </td><td>
-            <div class="download" id="do3"> <a  href="#"><img style="" src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/000zav-ysi-vid.png">Завантажити всі відео</a></div>
+            <div class="download" id="do3"> <a href="#"><img style="" src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/000zav-ysi-vid.png">Завантажити всі відео</a></div>
             </td><td>
                 <div class="download" id="do1">  <a href="#"><img style="" src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/000zav-ysi-vid2.png">Завантажити всі відео</a></div>
 </td></tr></table>
@@ -65,5 +134,22 @@ $this->widget('zii.widgets.CListView', array(
 
     <!-- lesson footer -->
 <?php $this->renderPartial('_lectureFooter', array('lecture'=>$lecture));?>
-
+<!--SingIn modal-->
+<?php
+$this->beginWidget('zii.widgets.jui.CJuiDialog', array(
+    'id' => 'mydialog2',
+    'themeUrl'=>Yii::app()->request->baseUrl.'/css',
+    'cssFile'=>'jquery-ui2.css',
+    'theme'=>'my',
+    'options' => array(
+        'width'=>540,
+        'autoOpen' => false,
+        'modal' => true,
+        'resizable'=> false
+    ),
+));
+$this->renderPartial('/lesson/_modalTask');
+$this->endWidget('zii.widgets.jui.CJuiDialog');
+?>
+<!--SignIn modal-->
 
