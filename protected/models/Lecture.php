6f46bb872ba197b1a866791d322f73e3bf44e2ad
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-05-04 11:07:40 +0300

    remove id in teacherInfo

--- protected/models/Lecture.php
+++ protected/models/Lecture.php
@@ -277,7 +277,6 @@ class Lecture extends CActiveRecord
             'skype' => $teacher->skype,
             'readMoreLink' => $teacher->readMoreLink,
             'photo' => $teacher->smallImage,
-            'id' => $teacher->teacher_id,
         );
     }
 
