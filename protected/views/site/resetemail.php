02cfb11aff6458d107cd63836ee9472b743a484d
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-05-13 15:59:16 +0300

    info about change email after emaillink

---
+++ protected/views/site/resetemail.php
@@ -0,0 +1,10 @@
+<?php
+$this->pageTitle = 'INTITA';
+?>
+<?php
+$this->breadcrumbs=array('Змінна email');
+?>
+<div style="margin:0 auto; margin-bottom: 400px; width: 540px; border: 1px solid #d8d8d8; border-radius: 5px;text-align: center">
+    <h1 style="color: #4b75a4;">Вітаємо!</h1>
+    <h2 style="color: #4b75a4;">Ви успішно змінили email</h2>
+</div>
