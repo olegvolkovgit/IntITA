76e0c5685439652dae3dfbfc1eb7f493330e8f54
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-05-15 11:55:04 +0300

    fix links

--- protected/views/course/_courseTeacher.php
+++ protected/views/course/_courseTeacher.php
@@ -16,19 +16,19 @@ for($i = 0; $i < count($teachers); $i++){
     <div id="teacher<?php echo $i;?>">
         <div class="courseTeacher">
             <div class="courseTeacherImg">
-                <a href="<?php echo Yii::app()->createUrl('profile');?>">
+                <a href="<?php echo Yii::app()->createUrl('profile/index', array('idTeacher' => $teachers[$i]->teacher_id));?>">
                     <img src="<?php echo Yii::app()->request->baseUrl . $teachers[$i]->foto_url; ?>" />
                 </a>
             </div>
             <div class="courseTeacherInfo">
-                <h3><a href="<?php echo Yii::app()->createUrl('profile');?>"><?php echo $teachers[$i]->last_name . " " . $teachers[$i]->first_name; ?></a></h3>
+                <h3><a href="<?php echo Yii::app()->createUrl('profile/index', array('idTeacher' => $teachers[$i]->teacher_id));?>"><?php echo $teachers[$i]->last_name . " " . $teachers[$i]->first_name; ?></a></h3>
                 <table class="courseTeacherDetail">
                     <?php
                     for($k = 0; $k < count($modules); $k++){
                         ?>
                         <tr>
                             <td>
-                                <span class="colorP"><?php echo Yii::t('course', '0208');  echo ' '.($k+1);?>: </span><span class="colorGrey"><a href="<?php echo Yii::app()->createUrl('module');?>"><?php echo $modules[$k]->module_name; ?></a></span>
+                                <span class="colorP"><?php echo Yii::t('course', '0208');  echo ' '.($k+1);?>: </span><span class="colorGrey"><a href="<?php echo Yii::app()->createUrl('module/index', array('idModule' => $modules[$k]->module_ID));?>"><?php echo $modules[$k]->module_name; ?></a></span>
                             </td>
                         </tr>
                     <?php
