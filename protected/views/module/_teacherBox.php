dee4a1b38d8135ba790d0861cb97e6557811bf92
todorovskiy <todorovskiy_dima@yahoo.com>
2015-05-15 16:15:03 +0300

    avatar circle page module

--- protected/views/module/_teacherBox.php
+++ protected/views/module/_teacherBox.php
@@ -5,11 +5,12 @@ for ($i = 0; $i < count($teachers); $i++) {
         <table>
             <tr>
                 <td class="teacherBoxLeft">
-                    <img src="<?php echo Yii::app()->request->baseUrl . $teachers[$i]->foto_url; ?>"/>
-
-                    <div style="height: 20px;">
-                        <a href="<?php echo Yii::app()->createUrl('profile/index', array('idTeacher' => $teachers[$i]->teacher_id));?>"><?php echo Yii::t('module', '0228'); ?>&#187;</a>
+                    <div
+                        style=" background: url(<?php echo Yii::app()->request->baseUrl . $teachers[$i]->foto_url; ?>) no-repeat; background-size: 90px; margin-left: 50px;">
+                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/common/img.png">
+                        <a href="<?php echo Yii::app()->createUrl('profile/index', array('idTeacher' => $teachers[$i]->teacher_id)); ?>"><?php echo Yii::t('module', '0228'); ?>&#187;</a>
                     </div>
+
                 </td>
                 <td class="teacherBoxRight"
                 ">
