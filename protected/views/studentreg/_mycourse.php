5aae66a715aeb8c81013225e885ac7d1453c2f66
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-05-15 11:54:20 +0300

    temp links

--- protected/views/studentreg/_mycourse.php
+++ protected/views/studentreg/_mycourse.php
@@ -4,7 +4,7 @@
         <tr>
             <td>
                 <p style="margin-left: 35px"><?php echo Yii::t('profile', '0118'); ?></p>
-                <a href="<?php echo Yii::app()->request->baseUrl; ?>/course"><span class="colorP">"Апгрейд головного мозку" </span></a>
+                <a href="<?php echo Yii::app()->request->baseUrl; ?>/course/index/1/"><span class="colorP">"Апгрейд головного мозку" </span></a>
             </td>
             <td>
                 <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/actualcourse.png"/>
@@ -16,17 +16,17 @@
     <p class="courseLevLang"><?php echo Yii::t('courses', '0069'); ?><span class="colorP"><?php echo Yii::t('profile', '0136'); ?></span></p>
 </div>
 <div class="profileModule">
-    <p><?php echo Yii::t('lecture', '0072'); ?></br> <a href="<?php echo Yii::app()->request->baseUrl; ?>/module"><span class="colorP">Модуль 4. Модульне око, модульний ніс</span></a></p>
+    <p><?php echo Yii::t('lecture', '0072'); ?></br> <a href="<?php echo Yii::app()->request->baseUrl; ?>/module/?idModule=1"><span class="colorP">Модуль 4. Модульне око, модульний ніс</span></a></p>
 </div>
 <div class="profileLesson">
-    <p><?php echo Yii::t('lecture', '0073'); ?></br> <a href="<?php echo Yii::app()->request->baseUrl; ?>/lesson"><span class="colorP">Заняття 6. Повторіння мать учіння</span></a></p>
+    <p><?php echo Yii::t('lecture', '0073'); ?></br> <a href="<?php echo Yii::app()->request->baseUrl; ?>/lesson/index/1/"><span class="colorP">Заняття 6. Повторіння мать учіння</span></a></p>
 </div>
 <div style="border-bottom:none" class="profileCourse">
     <table>
         <tr>
             <td>
                 <p style="margin-left: 35px"><?php echo Yii::t('profile', '0119'); ?></p>
-                <a href="<?php echo Yii::app()->request->baseUrl; ?>/course"><span class="colorP">"Як прокачати ельфа до 81-го рівня" </span></a>
+                <a href="<?php echo Yii::app()->request->baseUrl; ?>/course/index/2/"><span class="colorP">"Як прокачати ельфа до 81-го рівня" </span></a>
             </td>
             <td>
                 <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/incompletecourse.png"/>
@@ -43,7 +43,7 @@
         <tr>
             <td>
                 <p style="margin-left: 35px"><?php echo Yii::t('profile', '0120'); ?></p>
-                <a href="<?php echo Yii::app()->request->baseUrl; ?>/course"><span class="colorP">"Відкрий у собі гобліна"</span></a>
+                <a href="<?php echo Yii::app()->request->baseUrl; ?>/course/index/3/"><span class="colorP">"Відкрий у собі гобліна"</span></a>
             </td>
             <td>
                 <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/completecourse.png"/>
