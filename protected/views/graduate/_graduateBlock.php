eafd47fa422bd288ace75a3b3c6938ad2f6af0a3
todorovskiy <todorovskiy_dima@yahoo.com>
2015-05-23 13:51:42 +0300

    recallBlock page graduate

--- protected/views/graduate/_graduateBlock.php
+++ protected/views/graduate/_graduateBlock.php
@@ -3,6 +3,7 @@
         <tr>
             <td style="vertical-align: top;">
                 <img src="<?php echo StaticFilesHelper::createPath('image', 'graduates', $data['avatar']); ?>">
+
             </td>
 
             <td style="padding-left: 10px;">
@@ -11,8 +12,7 @@
 
                 <div class="text1"><?php echo $data['full_name'] ?></div>
 
-                <span class="spoilerLinks"><span class="spoilerClick">Розкрити відгук про навчання</span><span
-                        class="spoilerTriangle"> &#9660;</span></span>
+                <?php echo $this->renderPartial('_recallBlock'); ?>
 
                 <div class="text">Посада:&nbsp; <span style="color: #4B75A4;"><?php echo $data['position'] ?></span>
                 </div>
