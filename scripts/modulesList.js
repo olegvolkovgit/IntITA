d60eb9289c09dd1796c3eb36ae10116ccbca8388
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-05-28 19:20:47 +0300

    ajax buttons in add module

--- scripts/modulesList.js
+++ scripts/modulesList.js
@@ -4,6 +4,8 @@
 /**
  * Created by Ivanna on 04.05.2015.
  */
+$('.grid-view table.items td:first-child').hide();
+
 function showForm(){
     $form = document.getElementById('moduleForm');
     $form.style.display = 'block';
@@ -11,15 +13,21 @@ function showForm(){
 
 function enableEdit(){
     document.getElementById('editIco').style.display = 'none';
-    document.getElementById('addModuleButton').style.display = 'inline-block';
+    document.getElementById('addModule').style.display = 'inline-block';
     $('.grid-view table.items td:first-child').show();
 }
-
 function hideForm(id, title){
     $form = document.getElementById(id);
     $form.style.display = 'none';
     document.getElementById(title).innerHTML = '';
 }
+
+$(document).mouseup(function (e) {
+    var container = $('#moduleForm');
+    if (container.has(e.target).length === 0){
+        container.hide();
+    }
+});
 
 
 
