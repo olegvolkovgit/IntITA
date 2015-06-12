4d1b348eccd0daa52e95e335e2a7fd431d98dd58
IvannaBezpalko <vnnchkh@gmail.com>
2015-06-12 17:49:00 +0300

    Fixed bug on coursemanage page.

--- protected/views/coursemanage/_search.php
+++ protected/views/coursemanage/_search.php
@@ -81,11 +81,6 @@
 		<?php echo $form->textField($model,'course_img',array('size'=>60,'maxlength'=>255)); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->label($model,'review'); ?>
-		<?php echo $form->textArea($model,'review',array('rows'=>6, 'cols'=>50)); ?>
-	</div>
-
 	<div class="row buttons">
 		<?php echo CHtml::submitButton('Search'); ?>
 	</div>
