74b6cc3a80b9c398823805b22ccde76ae3f2f8b4
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-06-18 14:23:47 +0300

    tmanager validation, report 440, task block

--- protected/views/module/_addLessonForm.php
+++ protected/views/module/_addLessonForm.php
@@ -7,7 +7,7 @@
     <input name="idModule" value="<?php echo $newmodel->module_ID;?>" hidden="hidden">
     <input name="order" value="<?php echo $order+1;?>" hidden="hidden">
     <input name="lang" value="<?php echo $newmodel->language;?>" hidden="hidden">
-    <input type="text" name="newLectureName" id="newLectureName" required pattern="^[=а-яА-ЯёЁa-zA-Z0-9ЄєІі.,;: ()/+-]+$">
+    <input type="text" name="newLectureName" id="newLectureName" required pattern="^[=а-яА-ЯёЁa-zA-Z0-9ЄєІі.,;: ()/+-]+$" maxlength="255">
     <br><br>
     <input type="submit"  value=<?php echo Yii::t('module', '0383'); ?> id="submitButton" onclick="trimLectureName()">
 </form>
