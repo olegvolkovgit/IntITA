63d97efaf483764757386db8a2bb90cc8253a8fa
IvannaBezpalko <vnnchkh@gmail.com>
2015-06-22 16:53:31 +0300

    Update dump DB.

---
+++ int_ita_db/roles.sql
@@ -0,0 +1,36 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.21 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2015-06-22 16:52:29
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+-- Dumping structure for table int_ita_db.roles
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title_en` varchar(20) NOT NULL,
+  `title_ru` varchar(20) NOT NULL,
+  `title_ua` varchar(20) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COMMENT='List of teachers roles.';
+
+-- Dumping data for table int_ita_db.roles: ~7 rows (approximately)
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` (`id`, `title_en`, `title_ru`, `title_ua`, `description`) VALUES
+	(1, 'Trainer', 'Тренер', 'Тренер', ''),
+	(2, 'Consultant', 'Консультант', 'Консультант', 'Online consultations'),
+	(3, 'Leader', 'Руководитель проекта', 'Керівник проекта', 'Leader for single and team students projects'),
+	(4, 'Author', 'Автор модуля', 'Автор модуля', 'Module owner'),
+	(5, 'Admin', 'Админ', 'Адмін', 'Create and describe courses and modules.'),
+	(6, 'Teacher', 'Викладач', 'Викладач', 'Викладач'),
+	(7, 'teacher', 'Преподаватель', 'Викладач', 'description');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
