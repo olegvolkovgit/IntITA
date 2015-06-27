5b0da41dbca3428ee6fdad9d4feaab315ecda86b
IvannaBezpalko <vnnchkh@gmail.com>
2015-06-27 16:34:14 +0300

    Add atble course_modules

--- int_ita_db/element_type.sql
+++ int_ita_db/element_type.sql
@@ -3,7 +3,7 @@
 -- Server version:               5.6.21 - MySQL Community Server (GPL)
 -- Server OS:                    Win32
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2015-06-25 17:39:37
+-- Date/time:                    2015-06-27 16:33:13
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,9 +16,9 @@ CREATE TABLE IF NOT EXISTS `element_type` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `type` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='Types of lecture elements.';
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='Types of lecture elements.';
 
--- Dumping data for table int_ita_db.element_type: ~8 rows (approximately)
+-- Dumping data for table int_ita_db.element_type: ~11 rows (approximately)
 /*!40000 ALTER TABLE `element_type` DISABLE KEYS */;
 INSERT INTO `element_type` (`id`, `type`) VALUES
 	(1, 'text'),
@@ -29,7 +29,9 @@ INSERT INTO `element_type` (`id`, `type`) VALUES
 	(6, 'final task'),
 	(7, 'instruction'),
 	(8, 'label'),
-	(9, 'image');
+	(9, 'image'),
+	(10, 'formula'),
+	(11, 'table');
 /*!40000 ALTER TABLE `element_type` ENABLE KEYS */;
 /*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- int_ita_db/leader_modules.sql
+++ int_ita_db/leader_modules.sql
@@ -3,7 +3,7 @@
 -- Server version:               5.6.21 - MySQL Community Server (GPL)
 -- Server OS:                    Win32
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2015-06-25 17:39:38
+-- Date/time:                    2015-06-27 16:33:13
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
