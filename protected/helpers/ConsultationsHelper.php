7d7a88f8099e47ac45bb9ec20f37e7940fe8cf44
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-07-08 18:56:47 +0300

    letter update

--- protected/helpers/ConsultationsHelper.php
+++ protected/helpers/ConsultationsHelper.php
@@ -17,16 +17,14 @@ class ConsultationsHelper
     {
 
         $teacher = Teacher::model()->find("user_id=:user_id", array(':user_id'=>$id));
-
         if($teacher){
             if(StudentReg::model()->exists('id=:user', array(':user' => $dp->user_id))){
                 $result=StudentReg::model()->findByPk($dp->user_id)->firstName." ".StudentReg::model()->findByPk($dp->user_id)->secondName;
                 if($result==' ')
                     $result=StudentReg::model()->findByPk($dp->user_id)->email;
             }
-        } elseif(Teacher::model()->exists('teacher_id=:teacher', array(':teacher' => $dp->user_id))) {
+        } else
             $result = Teacher::model()->findByPk($dp->teacher_id)->first_name . " " . Teacher::model()->findByPk($dp->teacher_id)->last_name;
-        } else $result='unknown';
 
         return $result;
     }
