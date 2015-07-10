3a8cd53545059ebcd1fe4b1bd072b57af963a207
IvannaBezpalko <vnnchkh@gmail.com>
2015-07-10 16:42:18 +0300

    Merged forum branch, update DB

--- int_ita_db/messages.sql
+++ int_ita_db/messages.sql
@@ -3,7 +3,7 @@
 -- Server version:               5.6.21 - MySQL Community Server (GPL)
 -- Server OS:                    Win32
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2015-07-10 16:34:58
+-- Date/time:                    2015-07-10 16:41:41
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
--- int_ita_db/phpbb_styles.sql
+++ int_ita_db/phpbb_styles.sql
@@ -3,7 +3,7 @@
 -- Server version:               5.6.21 - MySQL Community Server (GPL)
 -- Server OS:                    Win32
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2015-07-10 16:34:59
+-- Date/time:                    2015-07-10 16:41:43
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
