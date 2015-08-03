0feb802d4dcd620d093c176f00145a4a79769b4e
IvannaBezpalko <vnnchkh@gmail.com>
2015-08-03 17:25:16 +0300

    Merged Ihor branch and update dump DB.

--- scripts/transition.js
+++ scripts/transition.js
@@ -1,11 +1,11 @@
 /**
  * Created by student on 30.05.2015.
  */
-$(document).onload ('click', 'a[href="/forum"]', function(){
+$(document).on ('click', 'a[href="/forum"]', function(){
     $.ajax({
         url: "/forum/forumEntrance.php",
         success: function (data) {
-            window.open('/IntITA/forum/index.php?transition=true', '_blank');
+            window.open('/forum/index.php?transition=true', '_blank');
         },
         error: function (error) {
             alert(JSON.stringify(error));
