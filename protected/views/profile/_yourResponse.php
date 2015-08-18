cabf8b164de0e1ca27c5956ebd8def612a7f4abf
Wizlight <Wizlightdragon@gmail.com>
2015-08-18 14:04:39 +0300

    hide some button in addblockredactor

--- protected/views/profile/_yourResponse.php
+++ protected/views/profile/_yourResponse.php
@@ -7,98 +7,100 @@
  */
 ?>
 <?php
-if($teacherRat && $teacherRat->knowledge!==null && $teacherRat->behavior!==null && $teacherRat->motivation!==null){
-    $knowldg= $teacherRat->knowledge;
-    $behvr=$teacherRat->behavior;
-    $motivtn=$teacherRat->motivation;
-    $knowval=$knowldg;
-    $behval=$behvr;
-    $motivval=$motivtn;
-} else{
-    $knowldg='0';
-    $behvr='0';
-    $motivtn='0';
-    $knowval=Null;
-    $behval=Null;
-    $motivval=Null;
+if ($teacherRat && $teacherRat->knowledge !== null && $teacherRat->behavior !== null && $teacherRat->motivation !== null) {
+    $knowldg = $teacherRat->knowledge;
+    $behvr = $teacherRat->behavior;
+    $motivtn = $teacherRat->motivation;
+    $knowval = $knowldg;
+    $behval = $behvr;
+    $motivval = $motivtn;
+} else {
+    $knowldg = '0';
+    $behvr = '0';
+    $motivtn = '0';
+    $knowval = Null;
+    $behval = Null;
+    $motivval = Null;
 }
 ?>
-<?php if(AccessHelper::canAddResponse()){?>
-<div class="lessonTask">
-    <img class="lessonBut" src="<?php echo StaticFilesHelper::createPath('image', 'teachers', 'lessButton.png');?>">
-    <div class="lessonButName" unselectable="on"><?php echo Yii::t('teacher', '0187'); ?></div>
-    <div class="lessonLine"></div>
-    <div class="responseBG">
+<?php if (AccessHelper::canAddResponse()) { ?>
+    <div class="lessonTask">
+        <img class="lessonBut"
+             src="<?php echo StaticFilesHelper::createPath('image', 'teachers', 'lessButton.png'); ?>">
 
-        <div>
-            <table style="padding-left: 35px; padding-top: 30px;">
-                <tr>
-                    <td align="right">
-                        <b><?php echo  Yii::t('teacher', '0188'); ?></b>
-                    </td>
-                </tr>
-                <tr>
-                    <td align="right">
-                        <?php echo Yii::t('teacher', '0189'); ?>
-                    </td>
-                    <td>
-                        <div id="material" ></div>
-                    </td>
-                </tr>
-                <tr>
-                    <td align="right">
-                        <?php echo Yii::t('teacher', '0190'); ?>
-                    </td>
-                    <td>
-                        <div id="behavior"></div>
-                    </td>
-                </tr>
-                <tr>
-                    <td align="right">
-                        <?php echo Yii::t('teacher', '0191'); ?>
-                    </td>
-                    <td>
-                        <div id="motiv"></div>
-                    </td>
-                </tr>
-            </table>
-        </div>
+        <div class="lessonButName" unselectable="on"><?php echo Yii::t('teacher', '0187'); ?></div>
+        <div class="lessonLine"></div>
+        <div class="responseBG">
 
-        <div class="BBCode">
-                <?php $form=$this->beginWidget('CActiveForm', array(
-                    'id'=>'response-form',
-                    'action'=> Yii::app()->createUrl('profile/index', array('idTeacher'=>$model->primaryKey)),
-                    'enableAjaxValidation'=>false,
+            <div>
+                <table style="padding-left: 35px; padding-top: 30px;">
+                    <tr>
+                        <td align="right">
+                            <b><?php echo Yii::t('teacher', '0188'); ?></b>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <?php echo Yii::t('teacher', '0189'); ?>
+                        </td>
+                        <td>
+                            <div id="material"></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <?php echo Yii::t('teacher', '0190'); ?>
+                        </td>
+                        <td>
+                            <div id="behavior"></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <?php echo Yii::t('teacher', '0191'); ?>
+                        </td>
+                        <td>
+                            <div id="motiv"></div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="BBCode">
+                <?php $form = $this->beginWidget('CActiveForm', array(
+                    'id' => 'response-form',
+                    'action' => Yii::app()->createUrl('profile/index', array('idTeacher' => $model->primaryKey)),
+                    'enableAjaxValidation' => false,
                     'htmlOptions' => array('enctype' => 'multipart/form-data'),
                 )); ?>
                 <div class="row">
-                    <?php echo $form->hiddenField($response,'knowledge',array('id'=>'rat1','value'=>$knowval)); ?>
+                    <?php echo $form->hiddenField($response, 'knowledge', array('id' => 'rat1', 'value' => $knowval)); ?>
                 </div>
                 <div class="row">
-                    <?php echo $form->hiddenField($response,'behavior',array('id'=>'rat2', 'value'=>$behval)); ?>
+                    <?php echo $form->hiddenField($response, 'behavior', array('id' => 'rat2', 'value' => $behval)); ?>
                 </div>
                 <div class="row">
-                    <?php echo $form->hiddenField($response,'motivation',array('id'=>'rat3','value'=>$motivval)); ?>
+                    <?php echo $form->hiddenField($response, 'motivation', array('id' => 'rat3', 'value' => $motivval)); ?>
                 </div>
 
                 <div class="row">
-                    <?php echo $form->textArea($response,'text', array('class'=>'editor')); ?>
+                    <?php echo $form->textArea($response, 'text', array('class' => 'editor', 'id' => 'go')); ?>
                 </div>
                 <div class="modelerrors">
-                    <?php if ($form->error($response,'knowledge') || $form->error($response,'behavior') ||  $form->error($response,'motivation'))
-                    echo Yii::t('response', '0385'); ?>
-                    <?php echo $form->error($response,'text'); ?>
+                    <?php if ($form->error($response, 'knowledge') || $form->error($response, 'behavior') || $form->error($response, 'motivation'))
+                        echo Yii::t('response', '0385'); ?>
+                    <?php echo $form->error($response, 'text'); ?>
                 </div>
                 <div class="rowbuttons">
                     <?php echo CHtml::submitButton(Yii::t('teacher', '0192'), array('id' => "sendResponse")); ?>
                 </div>
-                <?php if(Yii::app()->user->hasFlash('messageResponse')):
+                <?php if (Yii::app()->user->hasFlash('messageResponse')):
                     echo Yii::app()->user->getFlash('messageResponse');
                 endif; ?>
                 <?php $this->endWidget(); ?>
+            </div>
         </div>
     </div>
-</div>
 <?php } ?>
 <script type="text/javascript">
 
@@ -106,21 +108,37 @@ if($teacherRat && $teacherRat->knowledge!==null && $teacherRat->behavior!==null 
 
     $('#material').raty({
         score: <?php echo $knowldg; ?>,
-        click: function(score) {
+        click: function (score) {
             document.getElementById('rat1').value = score;
         }
     });
 
     $('#behavior').raty({
         score: <?php echo $behvr; ?>,
-        click: function(score) {
+        click: function (score) {
             document.getElementById('rat2').value = score;
         }
     });
     $('#motiv').raty({
         score: <?php echo $motivtn; ?>,
-        click: function(score) {
+        click: function (score) {
             document.getElementById('rat3').value = score;
+        }
+    });
+
+    $(document).ready(function () {
+        max = 500;
+        $('html').on('keydown', '.wysibb-text-editor', function () {
+            content = $(this);
+            $(this).keyup(function(e){ check_charcount($(this), max, e); });
+            $(this).keydown(function(e){ check_charcount($(this), max, e); });
+        });
+        function check_charcount(content, max, e)
+        {
+            if(e.which != 8 && $(content).html().length > max)
+            {
+                e.preventDefault();
+            }
         }
     });
 </script>
