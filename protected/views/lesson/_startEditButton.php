876fc06ee8c487c6acb41d828dabc46fe105ad3b
Wizlight <Wizlightdragon@gmail.com>
2015-08-21 13:11:51 +0300

    add preview button for teacher in edit lesson

--- protected/views/lesson/_startEditButton.php
+++ protected/views/lesson/_startEditButton.php
@@ -1,10 +1,17 @@
 <?php
-
+ if(isset($_GET['editPage'])) $page=$_GET['editPage'];
+else $page=0;
 ?>
 <div onclick="enableLessonEdit(1, <?php echo $_GET['idCourse'];?>);">
-    <a>
-        <img src="<?php echo StaticFilesHelper::createPath('image', 'editor', 'edt_30px.png'); ?>"
+    <div>
+        <img style="margin-left: 5px" src="<?php echo StaticFilesHelper::createPath('image', 'editor', 'edt_30px.png'); ?>"
              id="editIco<?php echo $block;?>" class="editButton" title="Список сторінок заняття"/>
-    </a>
+    </div>
+</div>
+<div onclick="enableLessonPreview(<?php echo $_GET['id'];?>, <?php echo $_GET['idCourse'];?>,<?php echo $page;?>);">
+    <div>
+        <img src="<?php echo StaticFilesHelper::createPath('image', 'editor', 'view.png'); ?>"
+             id="viewIco<?php echo $block;?>" class="editButton" title="Перегляд"/>
+    </div>
 </div>
 <br>
