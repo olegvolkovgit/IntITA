466f53812badb0387dda49cb0227a166a444b467
IvannaBezpalko <vnnchkh@gmail.com>
2015-08-28 16:43:39 +0300

    Admin panel (coursemanage)

--- protected/modules/_admin/views/default/index.php
+++ protected/modules/_admin/views/default/index.php
@@ -5,6 +5,7 @@
 
 <h2><a href="<?php echo Yii::app()->createUrl('/_admin/graduate/index');?>">Випускники</a></h2>
 <h2><a href="<?php echo Yii::app()->createUrl('/_admin/tmanage/index');?>">Викладачі</a></h2>
+<h2><a href="<?php echo Yii::app()->createUrl('/_admin/coursemanage/index');?>">Курси</a></h2>
 <h2><a href="<?php echo Yii::app()->createUrl('/_admin/permissions/index');?>">Права доступу</a></h2>
 
 <h2><a href="<?php echo Yii::app()->createUrl('/_admin/config/index');?>">Налаштування сайта</a></h2>
