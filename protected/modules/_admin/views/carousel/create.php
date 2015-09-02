29fad4911f58f2808da33c4517c906d0a0711ea0
IvannaBezpalko <vnnchkh@gmail.com>
2015-09-02 20:07:27 +0300

    Little fix

--- protected/modules/_admin/views/carousel/create.php
+++ protected/modules/_admin/views/carousel/create.php
@@ -1,16 +1,6 @@
 <?php
 /* @var $this CarouselController */
 /* @var $model Carousel */
-
-$this->breadcrumbs=array(
-	'Carousels'=>array('index'),
-	'Create',
-);
-
-$this->menu=array(
-	array('label'=>'List Carousel', 'url'=>array('index')),
-	array('label'=>'Manage Carousel', 'url'=>array('admin')),
-);
 ?>
 
 <h1>Create Carousel</h1>
