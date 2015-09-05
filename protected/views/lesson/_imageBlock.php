1555148913e6121a4b76e93d4519dae536c370f0
IvannaBezpalko <vnnchkh@gmail.com>
2015-09-05 11:03:42 +0300

    Fix lesson image

--- protected/views/lesson/_imageBlock.php
+++ protected/views/lesson/_imageBlock.php
@@ -14,6 +14,6 @@
         ));?>
 
         <div class="video" id="<?php echo "t" . $data['block_order'];?>" onclick="function(){order = this.id;}">
-            <img src="<?php echo Config::getBaseUrl().$data['html_block'];?>"  >
+            <img src="<?php echo $data['html_block'];?>"  >
         </div>
     </div>
