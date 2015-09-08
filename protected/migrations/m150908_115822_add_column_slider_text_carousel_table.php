4a8f221c1fa9231f993fe50517dc054b3504106f
IvannaBezpalko <vnnchkh@gmail.com>
2015-09-08 16:25:46 +0300

    Add migration (add column to carousel table). Fix lesson bug

--- protected/migrations/m150908_115822_add_column_slider_text_carousel_table.php
+++ protected/migrations/m150908_115822_add_column_slider_text_carousel_table.php
@@ -5,6 +5,12 @@ class m150908_115822_add_column_slider_text_carousel_table extends CDbMigration
 	public function up()
 	{
         $this->addColumn('carousel', 'slider_text', 'VARCHAR(6) NOT NULL');
+        Carousel::model()->updateByPk(1, array('slider_text' => "0027"));
+        Carousel::model()->updateByPk(2, array('slider_text' => "0028"));
+        Carousel::model()->updateByPk(3, array('slider_text' => "0029"));
+        Carousel::model()->updateByPk(4, array('slider_text' => "0030"));
+        Carousel::model()->updateByPk(5, array('slider_text' => "0559"));
+        Carousel::model()->updateByPk(6, array('slider_text' => "0560"));
 
 	}
 
--- protected/migrations/m150908_115912_add_translations_for_error_page.php
+++ protected/migrations/m150908_115912_add_translations_for_error_page.php
@@ -4,22 +4,63 @@ class m150908_115912_add_translations_for_error_page extends CDbMigration
 {
 	public function up()
 	{
+        $this->insert('sourcemessages', array(
+           'id' => '590',
+            'category' => 'error',
+            'message' => '0590'
+        ));
+        $this->insert('sourcemessages', array(
+            'id' => '591',
+            'category' => 'registration',
+            'message' => '0591'
+        ));
+        $this->insertMultiple('messages', array(
+            array(
+                'id_record' => null,
+                'id' => '0590',
+                'language' => 'ua',
+                'translation' => 'Помилка'
+            ),
+            array(
+                'id_record' => null,
+                'id' => '0590',
+                'language' => 'ru',
+                'translation' => 'Ошибка'
+            ),
+            array(
+                'id_record' => null,
+                'id' => '0590',
+                'language' => 'en',
+                'translation' => 'Error'
+            ),
+            array(
+                'id_record' => null,
+                'id' => '0591',
+                'language' => 'ua',
+                'translation' => 'Зареєструватися'
+            ),
+            array(
+                'id_record' => null,
+                'id' => '0591',
+                'language' => 'ru',
+                'translation' => 'Зарегистрироваться'
+            ),
+            array(
+                'id_record' => null,
+                'id' => '0591',
+                'language' => 'en',
+                'translation' => 'Sign up'
+            ),
+
+        ));
+
 	}
 
 	public function down()
 	{
-		echo "m150908_115912_add_translations_for_error_page does not support migration down.\n";
-		return false;
+        $this->delete('messages', 'id=590');
+        $this->delete('messages', 'id=591');
+        $this->delete('sourcemessages', 'id=590');
+        $this->delete('sourcemessages', 'id=591');
 	}
-
-	/*
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-	}
-
-	public function safeDown()
-	{
-	}
-	*/
 }
