92ab9390b56390477fdbebb3c22af39b076e1029
Wizlight <Wizlightdragon@gmail.com>
2015-09-12 16:03:29 +0300

    fix upload img in course

--- protected/modules/_admin/controllers/CoursemanageController.php
+++ protected/modules/_admin/controllers/CoursemanageController.php
@@ -5,7 +5,7 @@ class CoursemanageController extends CController
      * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
      * using two-column layout. See 'protected/views/layouts/column2.php'.
      */
-    public $layout='main';
+    public $layout = 'main';
 
     public function init()
     {
@@ -15,6 +15,7 @@ class CoursemanageController extends CController
             die();
         }
     }
+
     /**
      * @return array action filters
      */
@@ -25,65 +26,71 @@ class CoursemanageController extends CController
             'postOnly + delete', // we only allow deletion via POST request
         );
     }
+
     public function accessRules()
     {
         return array(
             array('allow',
-                'actions'=>array('delete', 'create', 'update', 'view', 'index', 'admin'),
-                'expression'=>array($this, 'isAdministrator'),
+                'actions' => array('delete', 'create', 'update', 'view', 'index', 'admin'),
+                'expression' => array($this, 'isAdministrator'),
             ),
             array('deny',
-                'message'=>"У вас недостатньо прав для перегляду та редагування сторінки.
+                'message' => "У вас недостатньо прав для перегляду та редагування сторінки.
                 Для отримання доступу увійдіть з логіном адміністратора сайту.",
-                'actions'=>array('delete', 'create', 'update', 'view', 'index', 'admin'),
-                'users'=>array('*'),
+                'actions' => array('delete', 'create', 'update', 'view', 'index', 'admin'),
+                'users' => array('*'),
             ),
         );
     }
+
     function isAdministrator()
     {
-        if(AccessHelper::isAdmin())
+        if (AccessHelper::isAdmin())
             return true;
         else
             return false;
     }
+
     /**
      * Displays a particular model.
      * @param integer $id the ID of the model to be displayed
      */
     public function actionView($id)
     {
-        $this->render('view',array(
-            'model'=>$this->loadModel($id),
+        $this->render('view', array(
+            'model' => $this->loadModel($id),
         ));
     }
+
     /**
      * Creates a new model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      */
     public function actionCreate()
     {
-        $model=new Course;
+        $model = new Course;
         // Uncomment the following line if AJAX validation is needed
         // $this->performAjaxValidation($model);
-        if(isset($_POST['Course']))
-        {
-            $_POST['Course']['course_img']=$_FILES['Course']['name']['course_img'];
-            $fileInfo=new SplFileInfo($_POST['Course']['course_img']);
-            $model->attributes=$_POST['Course'];
-            $model->logo=$_FILES['Course'];
-            if($model->save())
-                ImageHelper::uploadAndResizeImg(
-                    Yii::getPathOfAlias('webroot')."/images/course/".$_FILES['Course']['name']['course_img'],
-                    Yii::getPathOfAlias('webroot') . "/images/course/share/shareCourseImg_".$model->course_ID.'.'.$fileInfo->getExtension(),
-                    200
-                );
-            $this->redirect(array('view','id'=>$model->course_ID));
+        if (isset($_POST['Course'])) {
+            $_POST['Course']['course_img'] = $_FILES['Course']['name']['course_img'];
+            $fileInfo = new SplFileInfo($_POST['Course']['course_img']);
+            $model->attributes = $_POST['Course'];
+            $model->logo = $_FILES['Course'];
+            if ($model->save())
+                if (!empty($_POST['Course']['course_img'])) {
+                    ImageHelper::uploadAndResizeImg(
+                        Yii::getPathOfAlias('webroot') . "/images/course/" . $_FILES['Course']['name']['course_img'],
+                        Yii::getPathOfAlias('webroot') . "/images/course/share/shareCourseImg_" . $model->course_ID . '.' . $fileInfo->getExtension(),
+                        200
+                    );
+                }
+            $this->redirect(array('view', 'id' => $model->course_ID));
         }
-        $this->render('create',array(
-            'model'=>$model,
+        $this->render('create', array(
+            'model' => $model,
         ));
     }
+
     /**
      * Updates a particular model.
      * If update is successful, the browser will be redirected to the 'view' page.
@@ -91,28 +98,30 @@ class CoursemanageController extends CController
      */
     public function actionUpdate($id)
     {
-        $model=$this->loadModel($id);
+        $model = $this->loadModel($id);
         // Uncomment the following line if AJAX validation is needed
         // $this->performAjaxValidation($model);
-        if(isset($_POST['Course']))
-        {
-            $model->oldLogo=$model->course_img;
-            $_POST['Course']['course_img']=$_FILES['Course']['name']['course_img'];
-            $fileInfo=new SplFileInfo($_POST['Course']['course_img']);
-            $model->attributes=$_POST['Course'];
-            $model->logo=$_FILES['Course'];
-            if($model->save())
-                ImageHelper::uploadAndResizeImg(
-                    Config::getBaseUrl()."/images/course/".$_FILES['Course']['name']['course_img'],
-                    Config::getBaseUrl(). "/images/course/share/shareCourseImg_".$id.'.'.$fileInfo->getExtension(),
-                    200
-                );
-            $this->redirect(array('view','id'=>$model->course_ID));
+        if (isset($_POST['Course'])) {
+            $model->oldLogo = $model->course_img;
+            $_POST['Course']['course_img'] = $_FILES['Course']['name']['course_img'];
+            $fileInfo = new SplFileInfo($_POST['Course']['course_img']);
+            $model->attributes = $_POST['Course'];
+            $model->logo = $_FILES['Course'];
+            if ($model->save())
+                if (!empty($_POST['Course']['course_img'])) {
+                    ImageHelper::uploadAndResizeImg(
+                        Yii::getPathOfAlias('webroot') . "/images/course/" . $_FILES['Course']['name']['course_img'],
+                        Yii::getPathOfAlias('webroot') . "/images/course/share/shareCourseImg_" . $id . '.' . $fileInfo->getExtension(),
+                        200
+                    );
+                }
+            $this->redirect(array('view', 'id' => $model->course_ID));
         }
-        $this->render('update',array(
-            'model'=>$model,
+        $this->render('update', array(
+            'model' => $model,
         ));
     }
+
     /**
      * Deletes a particular model.
      * If deletion is successful, the browser will be redirected to the 'admin' page.
@@ -122,32 +131,35 @@ class CoursemanageController extends CController
     {
         Course::model()->updateByPk($id, array('cancelled' => 1));
         // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-        if(!isset($_GET['ajax']))
+        if (!isset($_GET['ajax']))
             $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
     }
+
     /**
      * Lists all models.
      */
     public function actionIndex()
     {
-        $dataProvider=new CActiveDataProvider('Course');
-        $this->render('index',array(
-            'dataProvider'=>$dataProvider,
+        $dataProvider = new CActiveDataProvider('Course');
+        $this->render('index', array(
+            'dataProvider' => $dataProvider,
         ));
     }
+
     /**
      * Manages all models.
      */
     public function actionAdmin()
     {
-        $model=new Course('search');
+        $model = new Course('search');
         $model->unsetAttributes();  // clear any default values
-        if(isset($_GET['Course']))
-            $model->attributes=$_GET['Course'];
-        $this->render('admin',array(
-            'model'=>$model,
+        if (isset($_GET['Course']))
+            $model->attributes = $_GET['Course'];
+        $this->render('admin', array(
+            'model' => $model,
         ));
     }
+
     /**
      * Returns the data model based on the primary key given in the GET variable.
      * If the data model is not found, an HTTP exception will be raised.
@@ -157,19 +169,19 @@ class CoursemanageController extends CController
      */
     public function loadModel($id)
     {
-        $model=Course::model()->findByPk($id);
-        if($model===null)
-            throw new CHttpException(404,'The requested page does not exist.');
+        $model = Course::model()->findByPk($id);
+        if ($model === null)
+            throw new CHttpException(404, 'The requested page does not exist.');
         return $model;
     }
+
     /**
      * Performs the AJAX validation.
      * @param Course $model the model to be validated
      */
     protected function performAjaxValidation($model)
     {
-        if(isset($_POST['ajax']) && $_POST['ajax']==='course-form')
-        {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'course-form') {
             echo CActiveForm::validate($model);
             Yii::app()->end();
         }
