cf49aaa94bf0b53091abbb553a964545ad64918c
IvannaBezpalko <vnnchkh@gmail.com>
2015-09-16 02:14:02 +0300

    Little fix

--- protected/migrations/m150915_225226_update_aboutus_table.php
+++ protected/migrations/m150915_225226_update_aboutus_table.php
@@ -4,12 +4,12 @@ class m150915_225226_update_aboutus_table extends CDbMigration
 {
 	public function up()
 	{
-        $this->addColumn('aboutus', 'titleText', 'VARCHAR(6) NOT NULL');
-        $this->addColumn('aboutus', 'textAbout', 'VARCHAR(6) NOT NULL');
+        //$this->addColumn('aboutus', 'titleText', 'VARCHAR(6) NOT NULL');
+        //$this->addColumn('aboutus', 'textAbout', 'VARCHAR(6) NOT NULL');
 
-        $this->update('step', array('titleText' => '0032', 'textAbout' => '0035'), 'blockID=1');
-        $this->update('step', array('titleText' => '0033', 'textAbout' => '0036'), 'blockID=2');
-        $this->update('step', array('titleText' => '0034', 'textAbout' => '0037'), 'blockID=3');
+        $this->update('aboutus', array('titleText' => '0032', 'textAbout' => '0035'), 'blockID=1');
+        $this->update('aboutus', array('titleText' => '0033', 'textAbout' => '0036'), 'blockID=2');
+        $this->update('aboutus', array('titleText' => '0034', 'textAbout' => '0037'), 'blockID=3');
 	}
 
 	public function down()
