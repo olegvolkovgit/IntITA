8258f8fdb31c47a538dadcb1a9cd94b37f747e77
IvannaBezpalko <vnnchkh@gmail.com>
2015-09-16 02:12:12 +0300

    Update mainpage (aboutus - view, db, controller).

--- protected/migrations/m150915_225226_update_aboutus_table.php
+++ protected/migrations/m150915_225226_update_aboutus_table.php
@@ -4,8 +4,8 @@ class m150915_225226_update_aboutus_table extends CDbMigration
 {
 	public function up()
 	{
-        $this->alterColumn('aboutus', 'titleText', 'VARCHAR(6) NOT NULL');
-        $this->alterColumn('aboutus', 'textAbout', 'VARCHAR(6) NOT NULL');
+        $this->addColumn('aboutus', 'titleText', 'VARCHAR(6) NOT NULL');
+        $this->addColumn('aboutus', 'textAbout', 'VARCHAR(6) NOT NULL');
 
         $this->update('step', array('titleText' => '0032', 'textAbout' => '0035'), 'blockID=1');
         $this->update('step', array('titleText' => '0033', 'textAbout' => '0036'), 'blockID=2');
