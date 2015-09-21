d520bc0532a3c6e1f7b3bdbc6a790c1632572397
IvannaBezpalko <vnnchkh@gmail.com>
2015-09-21 17:38:59 +0300

    Add column for forum table phpbb_topics

--- protected/migrations/m150921_142735_update_forum_topics_posts_tables_data.php
+++ protected/migrations/m150921_142735_update_forum_topics_posts_tables_data.php
@@ -2,14 +2,10 @@
 
 class m150921_142735_update_forum_topics_posts_tables_data extends CDbMigration
 {
-    public function init()
-    {
-        $this->db = 'dbForum';
-    }
 
     public function up()
 	{
-        //$this->db = 'dbForum';
+        $this->setDbConnection(Yii::app()->dbForum);
         $this->addColumn('phpbb_topics', 'lecture_id', 'MEDIUMINT(8) UNSIGNED NULL DEFAULT NULL');
 	}
 
