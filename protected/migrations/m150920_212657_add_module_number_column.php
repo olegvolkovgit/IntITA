6299917c36ec43791079fe300b7cc1d228c25c1e
IvannaBezpalko <vnnchkh@gmail.com>
2015-09-23 18:43:26 +0300

    Fix module numbers

--- protected/migrations/m150920_212657_add_module_number_column.php
+++ protected/migrations/m150920_212657_add_module_number_column.php
@@ -4,14 +4,14 @@ class m150920_212657_add_module_number_column extends CDbMigration
 {
 	public function safeUp()
 	{
-        //$this->addColumn('module', 'module_number', 'INT(10) NULL DEFAULT NULL');
+        $this->addColumn('module', 'module_number', 'INT(10) NULL DEFAULT NULL');
 
         $result = Yii::app()->db->createCommand()
             ->select('module_ID')
             ->from('module')
             ->queryAll();
         for ($i = 0, $count = count($result); $i < $count; $i++){
-            $this->update('module', array('module_ID' => $result[$i]), 'module_ID='.$result[$i]['module_ID']);
+            $this->update('module', array('module_number' => $result[$i]["module_ID"]), 'module_ID='.$result[$i]["module_ID"]);
         }
 	}
 
