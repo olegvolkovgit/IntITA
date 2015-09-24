c445cec274aa69461ad1df5f603e02f4bd2bffec
IvannaBezpalko <vnnchkh@gmail.com>
2015-09-24 16:43:10 +0300

    IS-137

--- scripts/graduate.js
+++ scripts/graduate.js
@@ -2,7 +2,7 @@ function deletePhoto(id, name){
     if (confirm('Ви впевнені, що хочете видалити фото випускника ' + name + '?')){
         $.ajax({
             type: "POST",
-            url: "/IntITA/_admin/graduate/deletePhoto",
+            url: "/_admin/graduate/deletePhoto",
             data: {'id': id},
             cache: false,
             success: function (data) {
