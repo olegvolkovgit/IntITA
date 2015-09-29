1175228191ad1b164f11fdcda91f78db7517e59f
Wizlight <Wizlightdragon@gmail.com>
2015-09-29 20:55:11 +0300

    UI-13

--- protected/views/courses/_menuLine.php
+++ protected/views/courses/_menuLine.php
@@ -6,22 +6,26 @@
  * Time: 15:46
  */
 ?>
+<?php if(isset($_GET['selector'])) $select=$_GET['selector']; else $select='all'; ?>
 <h1><?php echo Yii::t('courses', '0066'); ?></h1>
 
 <table>
     <tr>
         <!--Для початківців (6)   |   Для спеціалістів (0)   |   Для експертів (0)   |   Усі курси (6)   |-->
-        <td  valign="top"> <div class='sourse'><a href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'junior'));?>">
-                    <?php echo Yii::t('courses', '0140'); ?></a>&nbsp;(<?php echo count(Course::model()->findAllByAttributes(array('level' => array('junior','strong junior','intern'), 'language' => 'ua', 'cancelled'=>0))); ?>)</div></td>
+        <td  valign="top"> <div class='sourse <?php if($select=='junior') echo 'selectedSelector' ?>'><a href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'junior'));?>">
+                    <?php echo Yii::t('courses', '0140'); ?></a>&nbsp;<span class='courseNum'><?php echo count(Course::model()->findAllByAttributes(array('level' => array('junior','strong junior','intern'), 'language' => 'ua', 'cancelled'=>0))); ?></span></div></td>
+
         <td><div class='sourse'>&nbsp;&nbsp;<img src="<?php echo StaticFilesHelper::createPath('image', 'courses', 'coursesline2.png');?>"/>&nbsp;&nbsp;</div></td>
-        <td  valign="top"> <div class='sourse'><a href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'middle'));?>">
-                    <?php echo Yii::t('courses', '0141'); ?></a>&nbsp;(<?php echo Course::model()->count('level=:level and language=:lang and cancelled=0',	array(':level' => 'middle', ':lang' => 'ua'));?>)</div></td>
+        <td  valign="top"> <div class='sourse <?php if($select=='middle') echo 'selectedSelector' ?>'><a href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'middle'));?>">
+                    <?php echo Yii::t('courses', '0141'); ?></a>&nbsp;<span class='courseNum'><?php echo Course::model()->count('level=:level and language=:lang and cancelled=0',	array(':level' => 'middle', ':lang' => 'ua'));?></span></div></td>
+
         <td><div class='sourse'>&nbsp;&nbsp;<img src="<?php echo StaticFilesHelper::createPath('image', 'courses', 'coursesline2.png');?>"/>&nbsp;&nbsp;</div></td>
-        <td  valign="top"> <div class='sourse'><a href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'senior'));?>">
-                    <?php echo Yii::t('courses', '0142'); ?></a>&nbsp;(<?php echo Course::model()->count('level=:level and language=:lang and cancelled=0',	array(':level' => 'senior', ':lang' => 'ua'));?>)</div></td>
+        <td  valign="top"> <div class='sourse <?php if($select=='senior') echo 'selectedSelector' ?>'><a href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'senior'));?>">
+                    <?php echo Yii::t('courses', '0142'); ?></a>&nbsp;<span class='courseNum'><?php echo Course::model()->count('level=:level and language=:lang and cancelled=0',	array(':level' => 'senior', ':lang' => 'ua'));?></span></div></td>
+
         <td><div class='sourse'>&nbsp;&nbsp;<img src="<?php echo StaticFilesHelper::createPath('image', 'courses', 'coursesline2.png');?>"/>&nbsp;&nbsp;</div></td>
-        <td  valign="top"> <div class='sourse'><a href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'all'));?>">
-                    <?php echo Yii::t('courses', '0143'); ?></a>&nbsp;(<?php echo $total;?>)</div></td>
+        <td  valign="top"> <div class='sourse <?php if($select=='all') echo 'selectedSelector' ?>'><a href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'all'));?>">
+                    <?php echo Yii::t('courses', '0143'); ?></a>&nbsp;<span class='courseNum'><?php echo $total;?></span></div></td>
     </tr>
 </table>
 
