e4e10cffc3f155a2f41effb7ad4779fb36334961
Wizlight <Wizlightdragon@gmail.com>
2015-10-06 11:15:48 +0300

    is-173

--- protected/views/module/_addLessonForm.php
+++ protected/views/module/_addLessonForm.php
@@ -8,15 +8,15 @@
     <br>
     <br>
     <span>Назва (UA):</span>
-    <input type="text" name="titleUa" id="titleUa" required pattern="^[=а-яА-ЯёЁa-zA-Z0-9ЄєІіЇї.,<>:;`'?!~* ()/+-]+$" maxlength="255" size="70">
+    <input type="text" name="titleUa" id="titleUa" required pattern="^[=а-еж-щьюяА-ЕЖ-ЩЬЮЯa-zA-Z0-9ЄєІіЇї.,<>:;`'?!~* ()/+-]+$" maxlength="255" size="70">
     <br>
     <br>
     <span>Назва (RU):</span>
-    <input type="text" name="titleRu" id="titleRu" pattern="^[=а-яА-ЯёЁa-zA-Z0-9ЄєІіЇї.,<>:;`'?!~* ()/+-]+$" maxlength="255" size="70">
+    <input type="text" name="titleRu" id="titleRu" pattern="^[=а-яА-ЯёЁa-zA-Z0-9.,<>:;`'?!~* ()/+-]+$" maxlength="255" size="70">
     <br>
     <br>
     <span>Назва (EN):</span>
-    <input type="text" name="titleEn" id="titleEn" pattern="^[=а-яА-ЯёЁa-zA-Z0-9ЄєІіЇї.,<>:;`'?!~* ()/+-]+$" maxlength="255" size="70">
+    <input type="text" name="titleEn" id="titleEn" pattern="^[=zA-Z0-9.,<>:;`'?!~* ()/+-]+$" maxlength="255" size="70">
     <br><br>
     <input type="submit"  value=<?php echo Yii::t('module', '0383'); ?> id="submitButton" onclick="trimLectureName()">
 </form>
