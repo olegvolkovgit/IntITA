48caadeba6e79e146eca94d8df3a53e452a3a335
Quicks <1989alpan@gmail.com>
2015-10-22 16:18:38 +0300

    create interface BillableObject small clear ModuleHelper and CourseModule

---
+++ protected/models/accountancy/IBillableObject.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Quicks
+ * Date: 22.10.2015
+ * Time: 14:01
+ */
+interface IBillableObject
+{
+    public function getBasePrice();
+    public function getDuration();
+}
