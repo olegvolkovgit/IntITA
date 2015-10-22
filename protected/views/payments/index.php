a72978b9c9b8c18c38e2adc939af31f7f02a5061
Wizlight <Wizlightdragon@gmail.com>
2015-10-22 15:47:56 +0300

    fix footer in payments account

--- protected/views/payments/index.php
+++ protected/views/payments/index.php
@@ -1,31 +1,33 @@
-<script type="text/javascript" src="<?php echo Config::getBaseUrl();?>/scripts/jquery-1.8.3.js"></script>
+<script type="text/javascript" src="<?php echo Config::getBaseUrl(); ?>/scripts/jquery-1.8.3.js"></script>
 <link type="text/css" rel="stylesheet" href="<?php echo StaticFilesHelper::fullPathTo('css', 'account.css'); ?>"/>
-<script src="<?php echo StaticFilesHelper::fullPathTo('js', 'account.js');?>"></script>
+<script src="<?php echo StaticFilesHelper::fullPathTo('js', 'account.js'); ?>"></script>
 
 <head>
     <meta charset="UTF-8">
 </head>
 <div id="accountContainer">
-<?php $this->renderPartial('_account', array('account' => $account), false, true);?>
+    <?php $this->renderPartial('_account', array('account' => $account), false, true); ?>
     <div>
-<br>
-<br>
-<?php if (!isset($_GET['nolayout'])){ ?>
-    <button onclick="sendData('<?php echo $account->id_account; ?>')" id="printAccount">
-        <?php echo Yii::t('payment','0658');?>
-    </button>
-<?php } ?>
-<br>
-<br>
-<br>
-<?php if (isset($_GET['nolayout']) && $_GET['nolayout'] == 'true') { ?>
-    <script>
-        $(window).load(
-            function () {
-                window.print();
-            }
-        )
-    </script>
-<?php } ?>
-<script src="<?php echo StaticFilesHelper::fullPathTo('js', 'account.js');?>"></script>
+        <br>
+        <br>
+        <?php if (!isset($_GET['nolayout'])) { ?>
+            <button onclick="sendData('<?php echo $account->id_account; ?>')" id="printAccount">
+                <?php echo Yii::t('payment', '0658'); ?>
+            </button>
+        <?php } ?>
+        <br>
+        <br>
+        <br>
+        <?php if (isset($_GET['nolayout']) && $_GET['nolayout'] == 'true') { ?>
+            <script>
+                $(window).load(
+                    function () {
+                        window.print();
+                    }
+                )
+            </script>
+        <?php } ?>
+    </div>
+</div>
+<script src="<?php echo StaticFilesHelper::fullPathTo('js', 'account.js'); ?>"></script>
 <script src="<?php echo StaticFilesHelper::fullPathTo('js', 'jquery.cookie.js'); ?>"></script>
