e37407c36d9011530de52b7668fefcce620e9f2c
Wizlight <Wizlightdragon@gmail.com>
2015-10-24 13:04:02 +0300

    add translated

--- protected/migrations/m151024_085328_add_translation_672_673.php
+++ protected/migrations/m151024_085328_add_translation_672_673.php
@@ -10,6 +10,11 @@ class m151024_085328_add_translation_672 extends CDbMigration
 				'category' => 'error',
 				'message' => '0672'
 			),
+			array(
+				'id' => '673',
+				'category' => 'course',
+				'message' => '0673'
+			),
 		));
 
 		$this->insertMultiple('messages', array(
@@ -31,6 +36,24 @@ class m151024_085328_add_translation_672 extends CDbMigration
 				'language' => 'en',
 				'translation' => 'Invalid file format'
 			),
+			array(
+				'id_record' => null,
+				'id' => '0673',
+				'language' => 'ua',
+				'translation' => 'Εκηΰμεν'
+			),
+			array(
+				'id_record' => null,
+				'id' => '0673',
+				'language' => 'ru',
+				'translation' => 'έκηΰμεν'
+			),
+			array(
+				'id_record' => null,
+				'id' => '0673',
+				'language' => 'en',
+				'translation' => 'Exam'
+			),
 		));
 	}
 
@@ -38,5 +61,7 @@ class m151024_085328_add_translation_672 extends CDbMigration
 	{
 		$this->delete('messages', 'id=672');
 		$this->delete('sourcemessages', 'id=672');
+		$this->delete('messages', 'id=673');
+		$this->delete('sourcemessages', 'id=673');
 	}
 }
