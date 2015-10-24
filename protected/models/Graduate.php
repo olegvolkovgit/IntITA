db7ce435f1d6269005a6945f9eefd79289f43912
Wizlight <Wizlightdragon@gmail.com>
2015-10-24 14:49:31 +0300

    is-246

--- protected/models/Graduate.php
+++ protected/models/Graduate.php
@@ -43,6 +43,7 @@ class Graduate extends CActiveRecord
 			array('rate', 'numerical', 'integerOnly'=>true, 'message'=>'Рейтинг повинен бути числовим значенням.'),
 			array('first_name, last_name, avatar, position, work_place, work_site, history', 'length', 'max'=>255),
 			array('courses_page, first_name_en, last_name_en', 'length', 'max'=>50),
+			array('graduate_date', 'date', 'format' => 'yyyy-MM-dd','message'=>'Введіть дату в форматі РРРР-ММ-ДД'),
 			array('graduate_date, recall', 'safe'),
 			// The following rule is used by search().
 			array('id, first_name, last_name, avatar, graduate_date, position, work_place, work_site, courses_page, history, rate, recall, first_name_en, last_name_en', 'safe', 'on'=>'search'),
