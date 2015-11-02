55d9cc8e75bbf2dbd1399383ba288d942c080838
IvannaBezpalko <vnnchkh@gmail.com>
2015-11-02 15:12:07 +0200

    Little fix

--- protected/migrations/m151024_115736_add_modules_status.php
+++ protected/migrations/m151024_115736_add_modules_status.php
@@ -5,7 +5,7 @@ class m151024_115736_add_modules_status extends CDbMigration
 
 	public function safeUp()
 	{
-	    $this->addColumn('module', 'status', 'TINYINT DEFAULT 0');
+	    $this->addColumn('module', 'status', 'TINYINT(1) NOT NULL DEFAULT 0');
 	}
 
 	public function safeDown()
