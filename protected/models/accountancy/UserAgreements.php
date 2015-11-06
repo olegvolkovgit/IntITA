547d8b3115ab36530d1ec8f3309d6e6981fcc0ba
IvannaBezpalko <vnnchkh@gmail.com>
2015-11-06 17:55:44 +0200

    Little fix

--- protected/models/accountancy/UserAgreements.php
+++ protected/models/accountancy/UserAgreements.php
@@ -13,7 +13,7 @@
  * @property integer $cancel_user
  * @property string $cancel_date
  * @property string $close_date
- * @property string $payment_scheme
+ * @property string $payment_schema
  *
  * @property Service $service
  */
@@ -35,12 +35,13 @@ class UserAgreements extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('user_id, service_id, create_date, payment_scheme', 'required'),
+			array('user_id, service_id, create_date, payment_schema', 'required'),
 			array('user_id, approval_user, cancel_user', 'numerical', 'integerOnly'=>true),
-			array('service_id, payment_scheme', 'length', 'max'=>10),
+			array('service_id, payment_schema', 'length', 'max'=>10),
 			array('approval_date, cancel_date, close_date', 'safe'),
 			// The following rule is used by search().
-			array('id, user_id, service_id, create_date, approval_user, approval_date, cancel_user, cancel_date, close_date, payment_scheme', 'safe', 'on'=>'search'),
+			array('id, user_id, service_id, create_date, approval_user, approval_date, cancel_user, cancel_date,
+			close_date, payment_schema', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -71,7 +72,7 @@ class UserAgreements extends CActiveRecord
             'cancel_user' => 'Is agreement cancelled',
             'cancel_date' => 'date when agreement was cancelled',
             'close_date' => 'Date when agreement should be closed',
-            'payment_scheme' => 'Payment scheme',
+            'payment_schema' => 'Payment scheme',
 		);
 	}
 
@@ -102,7 +103,7 @@ class UserAgreements extends CActiveRecord
 		$criteria->compare('cancel_user',$this->cancel_user);
 		$criteria->compare('cancel_date',$this->cancel_date,true);
 		$criteria->compare('close_date',$this->close_date,true);
-		$criteria->compare('payment_scheme',$this->payment_scheme,true);
+		$criteria->compare('payment_schema',$this->payment_schema,true);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
