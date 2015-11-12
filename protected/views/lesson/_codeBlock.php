e23d6e7b54881dd4f762b4df84cabd034c64d3e9
Wizlight <Wizlightdragon@gmail.com>
2015-11-12 17:04:20 +0200

    fix toolbar

--- protected/views/lesson/_codeBlock.php
+++ protected/views/lesson/_codeBlock.php
@@ -7,7 +7,7 @@
  */
 ?>
 <div class="element">
-    <?php $this->renderPartial('/editor/_editToolbarCKE', array(
+    <?php $this->renderPartial('/editor/_editToolbar', array(
         'idLecture' => $data['id_lecture'],
         'order' =>  $data['block_order'],
         'editMode' => $editMode,
--- protected/views/lesson/_codeBlockCKE.php
+++ protected/views/lesson/_codeBlockCKE.php
@@ -7,7 +7,7 @@
  */
 ?>
 <div class="element">
-    <?php $this->renderPartial('/editor/_editToolbar', array(
+    <?php $this->renderPartial('/editor/_editToolbarCKE', array(
         'idLecture' => $data['id_lecture'],
         'order' =>  $data['block_order'],
         'editMode' => $editMode,
