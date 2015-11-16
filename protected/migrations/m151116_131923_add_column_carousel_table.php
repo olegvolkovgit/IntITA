ccb7fcfca416b36dc1088a5828bd18a18481a197
IvannaBezpalko <vnnchkh@gmail.com>
2015-11-16 17:56:24 +0200

    Fix carousel table migration

--- protected/migrations/m151116_131923_add_column_carousel_table.php
+++ protected/migrations/m151116_131923_add_column_carousel_table.php
@@ -2,26 +2,14 @@
 
 class m151116_131923_add_column_carousel_table extends CDbMigration
 {
-	public function up()
-	{
-        $this->dropPrimaryKey('order','carousel');
-        $this->addColumn('carousel','id','INT DEFAULT NULL');
-        $this->addPrimaryKey('id', 'carousel', 'id');
-	}
-
-	public function down()
-	{
-        $this->dropColumn('carousel', 'id');
-	}
-
-	/*
-	// Use safeUp/safeDown to do migration with transaction
 	public function safeUp()
 	{
+        $this->dropPrimaryKey('order','carousel');
+        $this->addColumn('carousel','id','pk');
 	}
 
 	public function safeDown()
 	{
+       $this->dropColumn('carousel', 'id');
 	}
-	*/
 }
