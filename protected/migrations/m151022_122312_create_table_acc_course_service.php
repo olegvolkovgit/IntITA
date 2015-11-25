9fbb249daec82d5a441d8a6d1951070c427c1489
IvannaBezpalko <vnnchkh@gmail.com>
2015-11-25 20:18:05 +0200

    Fix migrations

--- protected/migrations/m151022_122312_create_table_acc_course_service.php
+++ protected/migrations/m151022_122312_create_table_acc_course_service.php
@@ -7,19 +7,19 @@ class m151022_122312_create_table_acc_course_service extends CDbMigration
         $this->createTable('acc_course_service', array(
             'service_id' => 'INT(10) UNSIGNED NOT NULL',
             'course_id' => 'INT(11) NOT NULL',
-            'INDEX `service_id` (`service_id`, `course_id`)',
-            'INDEX `course_id` (`course_id`)',
-            'CONSTRAINT `FK_acc_course_service_acc_service` FOREIGN KEY (`service_id`) REFERENCES `acc_service` (`service_id`)',
-	        'CONSTRAINT `FK_acc_course_service_course` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_ID`)'
+//            'INDEX `service_id` (`service_id`, `course_id`)',
+//            'INDEX `course_id` (`course_id`)',
+//            'CONSTRAINT `FK_acc_course_service_acc_service` FOREIGN KEY (`service_id`) REFERENCES `acc_service` (`service_id`)',
+//	        'CONSTRAINT `FK_acc_course_service_course` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_ID`)'
         ));
 	}
 
 	public function safeDown()
 	{
-        $this->dropIndex('service_id', 'acc_course_service');
-        $this->dropIndex('course_id', 'acc_course_service');
-        $this->dropForeignKey('FK_acc_course_service_acc_service', 'acc_course_service');
-        $this->dropForeignKey('FK_acc_course_service_course', 'acc_course_service');
+//        $this->dropIndex('service_id', 'acc_course_service');
+//        $this->dropIndex('course_id', 'acc_course_service');
+//        $this->dropForeignKey('FK_acc_course_service_acc_service', 'acc_course_service');
+//        $this->dropForeignKey('FK_acc_course_service_course', 'acc_course_service');
         $this->dropTable('acc_course_service');
 	}
 }
