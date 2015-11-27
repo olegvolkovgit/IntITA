d7e1484d345163a3d2ba4e50f927e772760f9efa
Wizlight <Wizlightdragon@gmail.com>
2015-11-27 20:09:20 +0200

    change mibew size

--- protected/migrations/m151121_102054_add_verified_column.php
+++ protected/migrations/m151121_102054_add_verified_column.php
@@ -4,7 +4,7 @@ class m151121_102054_add_verified_column extends CDbMigration
 {
 	public function up()
 	{
-        $this->addColumn('lectures', 'verified', 'TINYINT(1) NOT NULL DEFAULT `0`');
+        $this->addColumn('lectures', 'verified', 'TINYINT(1) NOT NULL DEFAULT 0');
 	}
 
 	public function down()
