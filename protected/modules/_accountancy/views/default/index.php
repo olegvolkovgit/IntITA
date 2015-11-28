5c44668f5ebf170ad78d8268fa3f930f5d35579f
IvannaBezpalko <vnnchkh@gmail.com>
2015-11-28 12:51:25 +0200

    Little fix

--- protected/modules/_accountancy/views/default/index.php
+++ protected/modules/_accountancy/views/default/index.php
@@ -10,7 +10,7 @@
 </ul>
 <h2>Додаткові параметри</h2>
 <ul>
-    <li><?php echo CHtml::link('Типи операцій',array('/_accountancy/operationType/index')); ?>
+    <li><?php echo CHtml::link('Типи проплат',array('/_accountancy/operationType/index')); ?>
     <li><?php echo CHtml::link('Зовнішні джерела коштів',array('/_accountancy/externalSources/index')); ?>
-    <li><?php echo CHtml::link('Причини відміни операцій',array('/_accountancy/cancelReasonType/index')); ?>
+    <li><?php echo CHtml::link('Причини відміни проплат',array('/_accountancy/cancelReasonType/index')); ?>
 </ul>
