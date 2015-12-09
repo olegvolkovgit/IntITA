b761460158edafa15ec699da7ebfec1a7f96904f
IvannaBezpalko <vnnchkh@gmail.com>
2015-12-09 16:26:21 +0200

    Optimize teacher cabinet layout

--- protected/modules/_teacher/views/layouts/main.php
+++ protected/modules/_teacher/views/layouts/main.php
@@ -3,10 +3,6 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- for tabs -->
-    <!-- fonts -->
-    <link rel="stylesheet" href="<?php echo Config::getBaseUrl(); ?>/css/fontface.css"/>
-    <!-- fonts -->
     <link rel="shortcut icon" href="<?php echo Config::getBaseUrl(); ?>/css/images/favicon.ico" type="image/x-icon"/>
     <script src="<?= StaticFilesHelper::fullPathTo('js', 'jquery-1.11.3.min.js');?>"></script>
 </head>
