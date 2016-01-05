7582948f1252c7bfd037d830cc70e0e1ea2c8abd
Wizlight <Wizlightdragon@gmail.com>
2016-01-05 12:09:59 +0200

    change authorization form

--- protected/components/widgets/views/AuthorizationForm/authForm.php
+++ protected/components/widgets/views/AuthorizationForm/authForm.php
@@ -48,7 +48,7 @@
         </div>
         <div ng-show="signMode=='signIn'">
             <div class="authLinks">
-                <?php echo CHtml::link(Yii::t('regform', '0092'), '#', array('id' => 'authLinks', 'onclick' => 'openForgotpass()')); ?>
+                <?php echo CHtml::link(Yii::t('regform', '0092'), '', array('id' => 'authLinks', 'onclick' => 'openForgotpass()')); ?>
                 <label for="signUpMode" class=registration><?php echo Yii::t('registration', '0591'); ?></label>
                 <input ng-hide=true type="radio" ng-model="signMode" id="signUpMode" name="signMode" value="signUp" />
             </div>
