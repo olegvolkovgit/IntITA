9daeaea69f0a0873c5a492fa0b0d295989bdd211
Quicks <1989alpan@gmail.com>
2016-01-16 12:44:23 +0200

    Fix bug 521

--- protected/modules/_teacher/controllers/_admin/PermissionsController.php
+++ protected/modules/_teacher/controllers/_admin/PermissionsController.php
@@ -135,47 +135,35 @@ class PermissionsController extends TeacherCabinetController
 
     public function actionShowAttributes()
     {
-        $first = '<select size="1" name="attribute">';
         $criteria = new CDbCriteria();
         $criteria->select = 'id, name_ua';
         $criteria->order = 'id ASC';
         $criteria->addCondition('role=' . $_POST['role']);
         $rows = RoleAttribute::model()->findAll($criteria);
-        $result = $first . '<option value="">Всі атрибути ролі</option>
-                   <optgroup label="Виберіть атрибут">';
-
-        if (!empty($rows)) {
-            foreach ($rows as $numRow => $row) {
-                $result = $result . '<option value="' . $row['id'] . '">' . $row['name_ua'] . '</option>';
-            };
-        }
-        $last = '</select>';
-        $result .= $last;
-        echo $result;
+//        var_dump($rows);die;
+        $this->renderPartial('_showAttributes',array(
+            'rows' => $rows,
+        ),false,true);
     }
 
     public function actionShowAttributeInput()
     {
+        $attr = Yii::app()->request->getPost('attribute');
         $result = '';
-        switch ($_POST['attribute']) {
+        switch ($attr) {
             case '3':
             case '6':
             case '7':
-                $first = '<select name="attributeValue">';
-                $result = $first . '<option value="">' . Yii::t('payments', '0606') . '</option>
-                <optgroup label="' . Yii::t('payments', '0607') . '">';
-                $temp = Module::model()->findAll();
-                for ($i = 0; $i < count($temp); $i++) {
-                    $result = $result . '<option value="' . $temp[$i]->module_ID . '">' . $temp[$i]->module_number ."  ".
-                        $temp[$i]->title_ua ."(".$temp[$i]->language.")". '</option>';
-                }
-                $last = '</select>';
-                $result = $result.$last;
+                $modules = Module::model()->findAll();
+                $this->renderPartial('_showAttributeInput',array(
+                    'modules' => $modules,
+                ),false,true);
+
                 break;
             case 'user_list':
                 break;
             default:
-                $result .= "<br><br>Значення атрибута:  <input type='text' value='' name='attributeValue' id='inputValue'>";
+                $this->renderPartial('_showAttributeTextInput');
                 break;
         }
         echo $result;
@@ -230,10 +218,10 @@ class PermissionsController extends TeacherCabinetController
         $teacher = Teacher::model()->findByPk($teacherId);
         if ($teacherId && $roleId) {
             if ($teacher->setTeacherRole($roleId)) {
-                $this->redirect(Yii::app()->createUrl('/_admin/tmanage/index'));
+                $this->redirect(Yii::app()->createUrl('/_teacher/_admin/teachers/index'));
             }
         }
-        $this->redirect(Yii::app()->createUrl('/_teacher/_admin/roleAttribute/index'));
+        $this->redirect(Yii::app()->createUrl('/_teacher/_admin/teachers/index'));
     }
 
     public function actionSetTeacherRoleAttribute()
@@ -265,11 +253,11 @@ class PermissionsController extends TeacherCabinetController
 
             }
             if ($result) {
-                $this->redirect(Yii::app()->createUrl('/_teacher/_admin/roleAttribute/index'));
+                $this->redirect($this->pathToCabinet());
             }
 
         }
-        $this->redirect(Yii::app()->request->urlReferrer);
+        $this->redirect($this->pathToCabinet());
     }
 
     public function actionSetFreeLessons($id)
@@ -345,7 +333,7 @@ class PermissionsController extends TeacherCabinetController
         $role = Yii::app()->request->getPost('role');
 
         TeacherRoles::model()->deleteAllByAttributes(array('teacher' => $teacher, 'role' => $role));
-        $this->redirect('/_admin/tmanage/showRoles?id='.$teacher);
+        $this->redirect('/_teacher/_admin/teachers/showRoles',array('id' => $teacher));
     }
 
     public function actionShowUsers()
