d2cda425f23b1697cffa1f3fdcf1a12a5cc954f2
Wizlight <Wizlightdragon@gmail.com>
2016-01-22 19:30:23 +0200

    init Json

--- angular/js/interpreter_app/services/getTaskJson.js
+++ angular/js/interpreter_app/services/getTaskJson.js
@@ -5,18 +5,26 @@ angular
     .module('interpreterApp')
     .service('getTaskJson', [
         '$http',
-        function($http) {
-            this.getJson = function () {
-                $http({
+        function($http,$scope) {
+            this.getJson = function (lng,id) {
+                var json={
+                    "operation": "getJson",
+                    "lang": lng,
+                    "task": id
+                };
+                var promise = $http({
                     url: 'http://ii.intita.com',
                     method: "POST",
                     data: JSON.stringify(json),
                     headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}
                 }).then(function successCallback(response) {
-                    console.log(response.data);
+                    if(response.data.status=='success'){
+                        return response.data.json;
+                    }
                 }, function errorCallback() {
                     alert("Вибачте, але на сайті виникла помилка і додати задачу до заняття наразі неможливо. Спробуйте додати пізніше або зв'яжіться з адміністратором сайту.");
                 });
+                return promise;
             };
         }
     ]);
