29a23cf3bafa221a570fefbb9adb3df48d5b498d
Nicolas Haiduchok <nico13051995@gmail.com>
2016-01-22 13:58:25 +0200

    //add predis modul => index php redis config => aboutus

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+require('redis-session.php');
+# переопределяем префикс, под которым будут храниться сессии в
+# Redis. Это нужно сделать, т. к. в tomcat-redis-session-manager
+# захардкодили, что сессии хранятся без префикса
+define('REDIS_SESSION_PREFIX', '');
+# в Tomcat мы имя куки поменять не можем, поэтому меняем в PHP
+session_name('JSESSIONID');
+# собственно начинаем сессию
+RedisSession::start(array(
+    'host'     => '127.0.0.1', 
+    'password' => '1234567', 
+    'database' => 0, 
+));
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/protected/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
--- protected/views/aboutus/index.php
+++ protected/views/aboutus/index.php
@@ -1,7 +1,3 @@
-<? $css_version = 1; ?>
-    <link rel="stylesheet" type="text/css" href="<?php echo StaticFilesHelper::fullPathTo('css', 'aboutusstyles.css'); ?>"/>
-    <body onload=WindowShow(<?php echo (empty($_GET['id'])) ? 1 : $_GET['id']; ?>);>
-    </body>
 <?php
 $mainpage = new Mainpage();
 $headerText = $mainpage->getHeader1();
@@ -9,6 +5,23 @@ $subheaderText = $mainpage->getSubheader1();
 
  $this->renderPartial("_slider", array('slider' => $slider)); ?>
 
+ <http>
+	<!-- ... -->
+
+	<headers defaults-disabled="true">
+		<cache-control/>
+		<frame-options policy="SAMEORIGIN" />
+		<hsts disable="true"/>
+	</headers>
+</http>
+
+
+       <iframe  src="http://localhost:8080" scrolling="no" id="idIframe"  width="100%" height=3000px  style="border:0" onLoad="resizeIframe();"></iframe>
+	   <? $css_version = 1; ?>
+    <link rel="stylesheet" type="text/css" href="<?php echo StaticFilesHelper::fullPathTo('css', 'aboutusstyles.css'); ?>"/>
+    <body onload=WindowShow(<?php echo (empty($_GET['id'])) ? 1 : $_GET['id']; ?>);>
+    </body>
+ 
 <?php $this->renderPartial('_shortBlocks', array('massAbout' => $arrayAboutUs)); ?>
 <?php $this->renderPartial('_aboutDetail', array(
     'block1' => $arrayAboutUs[0],
