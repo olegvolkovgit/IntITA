89d17fb0687036bfb461da3aac23885f3fedd92a
IvannaBezpalko <vnnchkh@gmail.com>
2016-02-03 18:26:05 +0200

    Minor fix

--- css/dist/js/sb-admin-2.js
+++ css/dist/js/sb-admin-2.js
@@ -1,11 +1,11 @@
-$(function() {
+$jq(function() {
     $jq('#side-menu').metisMenu();
 });
 
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
-$(function() {
+$jq(function() {
     $(window).bind("load resize", function() {
         topOffset = 50;
         width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
