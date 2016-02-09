eb381944fcbc40e6bf7393f3068a423d23af30ee
Inna Kocherzhuk <innakocherzhuk@gmail.com>
2016-02-09 14:00:34 +0200

    corrected elements size

--- protected/modules/_teacher/views/trainer/showPlainTask.php
+++ protected/modules/_teacher/views/trainer/showPlainTask.php
@@ -11,33 +11,34 @@
 $mark = $plainTask->mark();
 ?>
 
-<div class="col-md-8" >
+<div class="col-md-8">
     <div class="panel panel-default">
         <div class="panel-heading">
             <h3 class="panel-title">Оцінка простої задачі</h3>
         </div>
         <div class="panel-body">
-            <input type="text" id="plainTaskId" hidden="hidden" value="<?php echo $plainTask->id; ?>">
-            <input type="text" id="userId" hidden="hidden" value="<?php echo $plainTask->id_student; ?>">
+            <div class="row">
+                <input type="text" id="plainTaskId" hidden="hidden" value="<?php echo $plainTask->id; ?>">
+                <input type="text" id="userId" hidden="hidden" value="<?php echo $plainTask->id_student; ?>">
 
-            <div class="form-group">
+            <div class="form-group col-md-10">
                 <label for="fromWho">Від кого</label>
                 <input type="text" class="form-control" id="fromWho"
                        placeholder="<?php echo $plainTask->getStudentName() ?>" readonly>
             </div>
-            <div class="form-group">
+            <div class="form-group col-md-10">
                 <label for="condition">Задача</label>
                 <textarea class="form-control" name="condition" id="textareaSettingsbyId"
                           readonly><?php echo $plainTask->getCondition() ?>
                 </textarea>
             </div>
-            <div class="form-group">
+            <div class="form-group col-md-10">
                 <label for="answer">Відповідь</label>
                 <textarea class="form-control" name="answer" id="textareaSettingsbyId"
                           readonly><?php echo $plainTask->answer; ?>
                 </textarea>
             </div>
-            <div class="form-group">
+            <div class="form-group col-md-10">
                 <label for="mark">Оцінка</label>
                 <select class="form-control" id="mark">
                     <option value="0">не зараховано</option>
@@ -45,14 +46,16 @@ $mark = $plainTask->mark();
                 </select>
             </div>
 
-            <div class="form-group">
+            <div class="form-group col-md-10">
                 <label for="comment">Коментар до задачі</label>
                 <textarea class="form-control" name="comment" id="textareaSettingsbyId"><?=$mark['comment'];?></textarea>
             </div>
+                <div class="col-md-3">
             <button onclick="markPlainTask('<?php echo Yii::app()->createUrl('/_teacher/teacher/markPlainTask') ?>')"
                     class="btn btn-primary"><?php echo ($mark['mark'])?'Змінити оцінку':'Оцінити';?>
             </button>
-
+                </div>
+        </div>
         </div>
     </div>
 </div>
