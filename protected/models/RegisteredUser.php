1f57eef3bb48da7c72143157efc25b35cdd3d21b
Oleksii Tiurenkov <alterego4@gmail.com>
2016-02-14 17:26:45 +0200

    Added roles array

--- protected/models/RegisteredUser.php
+++ protected/models/RegisteredUser.php
@@ -16,6 +16,7 @@ class RegisteredUser
     //put your code here
     //StudentReg variable
     public $registrationData;
+    private $_roles;
     
     public function __construct(StudentReg $registrationData) 
     {
@@ -43,4 +44,30 @@ class RegisteredUser
     {
         return $this->registrationData->$name;
     }
+    
+    public function getRoles()
+    {
+        if ($this->_roles === null) {
+            
+                $this->_roles = $this->loadRoles();
+        }
+        return $this->_roles;
+    }
+    
+    private function loadRoles(){
+        $sql = '(select "admin",id_user from user_admin a where a.id_user = '.$this->id.' and end_date IS NULL)
+                    union
+                (select "accountant", id_user from user_accountant ac where ac.id_user = '.$this->id.' and end_date IS NULL)
+                    union
+                (select "student", id_user from user_student st where st.id_user = '.$this->id.' and end_date IS NULL)
+                     union
+                (select "trainer", id_user from user_trainer at where at.id_user = '.$this->id.' and end_date IS NULL)
+                     union
+                (select "consultant", id_user from user_consultant acs where acs.id_user = '.$this->id.' and end_date IS NULL)';
+        $rolesArray = Yii::app()->db->createCommand($sql)->queryAll();
+
+        $result = array_map(function($row){return new UserRoles($row[0]);}, $rolesArray);
+        return $result;
+    }
+    
 }
