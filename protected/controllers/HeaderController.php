63302ae8c0315976cb8542d16d804ea7d95888f4
IvannaBezpalko <vnnchkh@gmail.com>
2016-02-16 13:13:05 +0200

    Delete unused files. Minor fix PaymentsController (used INTITAUser model)

--- protected/controllers/HeaderController.php
+++ protected/controllers/HeaderController.php
@@ -2,16 +2,6 @@
 
 class HeaderController extends Controller
 {
-//    public function filters()
-//    {
-//        return array(
-//            array(
-//                'COutputCache',
-//                'duration'=> 60,
-//            ),
-//        );
-//    }
-
 	public function actionIndex()
 	{
 		$this->render('index');
