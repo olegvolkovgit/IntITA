a5d8b8d09b8f2e0d0ce048a6590879484bfc5c53
Inna Kocherzhuk <innakocherzhuk@gmail.com>
2016-02-24 20:27:31 +0200

    utf-8

--- protected/views/mail/templates/_registrationMail.php
+++ protected/views/mail/templates/_registrationMail.php
@@ -5,9 +5,9 @@
  */
 $model = $params[0];
 ?>
-<h4>Вітаємо!</h4>
+<h4>Р’С–С‚Р°С”РјРѕ!</h4>
 Yii::t('activeemail', '0299')
 <span>
 <a href="<?=Yii::app()->createAbsoluteUrl('/index.php?r=site/AccActivation/view&token=', array($model->token,$model->token, $lang, $model->email));?>"> .</span>
 <br>
-?З повагою, INTITA?;
+вЂ‹Р— РїРѕРІР°РіРѕСЋ, INTITAвЂ‹;
