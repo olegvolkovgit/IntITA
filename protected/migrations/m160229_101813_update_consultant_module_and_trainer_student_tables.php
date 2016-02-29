2d43d70f604ddef47cbff644fc59d764cf8dce89
IvannaBezpalko <vnnchkh@gmail.com>
2016-02-29 15:33:35 +0200

    Fix migration

--- protected/migrations/m160229_101813_update_consultant_module_and_trainer_student_tables.php
+++ protected/migrations/m160229_101813_update_consultant_module_and_trainer_student_tables.php
@@ -7,8 +7,8 @@ class m160229_101813_update_consultant_module_and_trainer_student_tables extends
         $sqlConsultant = "update consultant_modules set consultant = (select user_id from teacher where teacher_id=consultant);";
         $sqlTrainer = "update trainer_student set trainer = (select user_id from teacher where teacher_id=trainer);";
 
-        //$this->dropForeignKey('FK_consultant_modules_teacher', 'consultant_modules');
-        //$this->dropForeignKey('FK_trainer_student_teacher', 'trainer_student');
+        $this->dropForeignKey('FK__teacher', 'consultant_modules');
+        $this->dropForeignKey('FK_trainer_student_teacher', 'trainer_student');
         $this->execute($sqlConsultant);
         $this->execute($sqlTrainer);
     }
