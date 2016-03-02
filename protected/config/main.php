4b2aa91302433de3c9bdbd37839c96aec79500e9
RidgeA <antongriadchenko@gmail.com>
2016-03-02 19:40:09 +0200

    Added text blocks version control.

--- protected/config/main.php
+++ protected/config/main.php
@@ -173,5 +173,6 @@ return array(
         // this is used in teacher profile page
         'adminEmail' => 'Wizlightdragon@gmail.com',
         'languages' => array('en' => 'English', 'ua' => 'Ukrainian', 'ru' => 'Russian'),
+        'dbDateFormat'=>'Y-m-d H:m:s'
     ),
 );
