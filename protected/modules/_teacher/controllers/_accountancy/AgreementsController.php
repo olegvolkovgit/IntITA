c10425cdb1e5a9a4a2e06b2046d81b7f9e48d1ce
IvannaBezpalko <vnnchkh@gmail.com>
2016-03-16 18:03:10 +0200

    Fix IS-1113, deleted unused actions.

--- protected/modules/_teacher/controllers/_accountancy/AgreementsController.php
+++ protected/modules/_teacher/controllers/_accountancy/AgreementsController.php
@@ -15,65 +15,6 @@ class AgreementsController extends TeacherCabinetController
     }
 
     /**
-     * Creates a new model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     */
-    public function actionCreate()
-    {
-        $model = new UserAgreements;
-
-        // Uncomment the following line if AJAX validation is needed
-        // $this->performAjaxValidation($model);
-
-        if (isset($_POST['UserAgreements'])) {
-            $model->attributes = $_POST['UserAgreements'];
-            if ($model->save())
-                $this->redirect(array('view', 'id' => $model->id));
-        }
-
-        $this->renderPartial('create', array(
-            'model' => $model,
-        ));
-    }
-
-    /**
-     * Updates a particular model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id the ID of the model to be updated
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->loadModel($id);
-
-        // Uncomment the following line if AJAX validation is needed
-        // $this->performAjaxValidation($model);
-
-        if (isset($_POST['UserAgreements'])) {
-            $model->attributes = $_POST['UserAgreements'];
-            if ($model->save())
-                $this->redirect(array('view', 'id' => $model->id));
-        }
-
-        $this->renderPartial('update', array(
-            'model' => $model,
-        ));
-    }
-
-    /**
-     * Deletes a particular model.
-     * If deletion is successful, the browser will be redirected to the 'admin' page.
-     * @param integer $id the ID of the model to be deleted
-     */
-    public function actionDelete($id)
-    {
-        $this->loadModel($id)->delete();
-
-        // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-        if (!isset($_GET['ajax']))
-            $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-    }
-
-    /**
      * Returns the data model based on the primary key given in the GET variable.
      * If the data model is not found, an HTTP exception will be raised.
      * @param integer $id the ID of the model to be loaded
@@ -129,10 +70,11 @@ class AgreementsController extends TeacherCabinetController
                 'cancel_user' => Yii::app()->user->getId(),
                 'cancel_date' => date("Y-m-d H:i:s"),
             )))
-                return "success";
-            else return "fail";
+                echo "Договір ".$id." скасований.";
+            else echo "Договір ".$id." не вдалося скасувати. Спробуйте пізніше або зверніться до адміністратора "
+            .Config::getAdminEmail();
         } else {
-            throw new CHttpException(403, "Договір ще не підтверджений. Ви не можете його закрити.");
+           echo "Договір ще не підтверджений. Ви не можете його закрити.";
         }
     }
 
