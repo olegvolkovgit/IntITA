31b857f97623c9722e2c967772f4220d6b3f0a0e
IvannaBezpalko <vnnchkh@gmail.com>
2016-03-25 12:56:44 +0200

    Minor migration fix.

--- protected/migrations/m160318_100815_add_time_columns_plain_task_answer_teacher_table.php
+++ protected/migrations/m160318_100815_add_time_columns_plain_task_answer_teacher_table.php
@@ -6,9 +6,6 @@ class m160318_100815_add_time_columns_plain_task_answer_teacher_table extends CD
 	{
 		$this->addColumn('plain_task_answer_teacher', 'start_date', 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP');
         $this->addColumn('plain_task_answer_teacher', 'end_date', 'DATETIME NULL');
-        //add primary key (with deleting duplicates) in table plain_task_answer_teacher
-        $sql = "ALTER IGNORE TABLE plain_task_answer_teacher PRIMARY KEY (id_plain_task_answer, start_date);";
-        $this->execute($sql);
 	}
 
 	public function safeDown()
