0994814d5f68015d7f8425c614cab4ab2fa164e4
IvannaBezpalko <vnnchkh@gmail.com>
2016-03-28 14:39:13 +0300

    Improving config table.

--- protected/views/mail/templates/_assignedConsultantLetter.php
+++ protected/views/mail/templates/_assignedConsultantLetter.php
@@ -9,6 +9,6 @@ $plainTaskAnswer = $params[0];
 <span>У Тебе з'явилася нова задача для перевірки : <strong><?= $plainTaskAnswer->answer;?></strong>.</span>
 <br>
 <span>Щоб продивитися нову задачу, перейди за посиланням:</span>
-<a href="<?=Yii::app()->createAbsoluteUrl('_teacher/teacher/checkPlainTaskAnswer',array('id' => $plainTaskAnswer->id_plain_task));?>">Задача до перевірки</a>
+<a href="<?=Yii::app()->createAbsoluteUrl('/_teacher/teacher/checkPlainTaskAnswer',array('id' => $plainTaskAnswer->id_plain_task));?>">Задача до перевірки</a>
 <br>
 ​З повагою, INTITA​;
