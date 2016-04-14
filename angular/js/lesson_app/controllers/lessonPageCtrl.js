fa44efd9cf5f6baa653ef91a3784d6b8251bbcf6
Morgunov <morgunov.igor1990@gmail.com>
2016-04-14 21:22:59 +0300

    added ability write history of visit pages "video","quiz","text" into DB

--- angular/js/lesson_app/controllers/lessonPageCtrl.js
+++ angular/js/lesson_app/controllers/lessonPageCtrl.js
@@ -23,7 +23,12 @@ function lessonPageCtrl($rootScope,$scope, ipCookie,openDialogsService) {
                 ipCookie('lessonTab', 0, { path: '/' });
                 break;
             case 'text':
+                $scope.startLogText();
                 ipCookie('lessonTab', 1, { path: '/' });
+                break;
+            case 'quiz':
+                $scope.startLogQuiz();
+                ipCookie('lessonTab', 2, { path: '/' });
                 break;
         }
     });
@@ -48,8 +53,15 @@ function lessonPageCtrl($rootScope,$scope, ipCookie,openDialogsService) {
             $('#ui-id-'+tab+'').click();
         }
     };
+    $scope.startLogQuiz=function(){
+        $.post("/track/index", {  events: "quiz", lesson: idLecture,part: $rootScope.currentPage } );
 
-    $scope.startLog=function(){
+    };
+    $scope.startLogText=function(){
+        $.post("/track/index", {  events: "text", lesson: idLecture,part: $rootScope.currentPage } );
+
+    };
+    $scope.startLogVideo=function(){
       $.post("/track/index", {  events: "start_video", lesson: idLecture,part: $rootScope.currentPage } );
 
        }
