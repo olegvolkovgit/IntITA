91230057971628db01f47a6353a3244db925e027
RidgeA <antongriadchenko@gmail.com>
2016-04-17 12:52:34 +0300

    Fixed main.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -148,12 +148,6 @@ return array(
                     'levels'=>'error, warning, trace, profile, info',
                     'showInFireBug' => true
                 ),
-                array(
-                    'class'=>'CFileLogRoute',
-                    'levels'=>'error, warning, trace, info, profile',
-                    'categories'=>'system.db.*',
-                    'logFile'=>'sql.log',
-                ),
             ),
         ),
 
