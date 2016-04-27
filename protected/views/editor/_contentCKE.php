473799d16e90a5e3ca4783437abeaf7cbe7ddb2c
IvannaBezpalko <vnnchkh@gmail.com>
2016-04-27 20:41:52 +0300

    Minor lecture view refactor.

--- protected/views/editor/_contentCKE.php
+++ protected/views/editor/_contentCKE.php
@@ -1,32 +1,24 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Ivanna
- * Date: 12.04.2015
- * Time: 2:05
+ * @var $data LectureElement
  */
 ?>
 <br>
 <?php
-    for ($i = count($data); $i > 0; $i--){
-
-        switch ($data['id_type']){
-            case 1:
-                $this->renderPartial('/editor/_textBlockCKE', array('data'=>$data, 'editMode' => $editMode));
-                break;
-            case 3:
-                $this->renderPartial('/editor/_codeBlockCKE', array('data'=>$data, 'editMode' => $editMode));
-                break;
-            case 4:
-                $this->renderPartial('/editor/_exampleBlockCKE', array('data'=>$data, 'editMode' => $editMode));
-                break;
-            case 7:
-                $this->renderPartial('/editor/_instructionBlockCKE', array('data'=>$data, 'editMode' => $editMode));
-                break;
-            case 13:
-                break;
-            default:
-                break;
-        }
-    }
+switch ($data->id_type) {
+    case LectureElement::TEXT:
+        $this->renderPartial('/editor/_textBlockCKE', array('data' => $data, 'editMode' => $editMode));
+        break;
+    case LectureElement::CODE:
+        $this->renderPartial('/editor/_codeBlockCKE', array('data' => $data, 'editMode' => $editMode));
+        break;
+    case LectureElement::EXAMPLE:
+        $this->renderPartial('/editor/_exampleBlockCKE', array('data' => $data, 'editMode' => $editMode));
+        break;
+    case LectureElement::INSTRUCTION:
+        $this->renderPartial('/editor/_instructionBlockCKE', array('data' => $data, 'editMode' => $editMode));
+        break;
+    default:
+        break;
+}
 ?>
