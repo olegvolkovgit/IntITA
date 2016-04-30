3c61352afebf5e4981d4d4f1be26496c6183e098
IvannaBezpalko <vnnchkh@gmail.com>
2016-04-30 13:06:25 +0300

    Fix course page side block appearing.

--- protected/views/course/_sideBlock.php
+++ protected/views/course/_sideBlock.php
@@ -9,21 +9,30 @@
     <?php if ($model->$param != '') { ?>
         <p class="subCourseInfo"><b><?php echo Yii::t('course', '0204'); ?></b></p>
         <?php
-        $forWhomArray = explode(";", $model->$param);
-        ?>
-        <li id="<?=$param?>Item"><?= (strlen($forWhomArray[0]) <= 60)?$forWhomArray[0]:mb_substr($forWhomArray[0], 0, 60, 'UTF-8')."..."; ?>
-            <a href="#" onclick="showBlock('#<?=$param?>Block', '#<?=$param?>Item', '#linkDetail<?=$param?>'); return false;">детальніше...</a></li>
-        <div id="<?=$param?>Block" style="display: none">
-            <?php
-            foreach ($forWhomArray as $key => $item) {
-                ?>
-                <li><?php echo $item . ";"; ?></li>
-                <?php
-            }
+        if(strlen($model->$param) < 75){?>
+            <li><?php echo $model->$param; ?></li>
+        <?php }
+        else {
+            $forWhomArray = explode(";", $model->$param);
             ?>
-            <a id="linkDetail<?=$param?>" href="#" onclick="hideBlock('#<?=$param?>Block', '#<?=$param?>Item', '#linkDetail<?=$param?>');  return false;">
-                приховати
-            </a>
-        </div>
-    <?php } ?>
+            <li id="<?= $param ?>Item"><?= (strlen($forWhomArray[0]) <= 60) ? $forWhomArray[0] : mb_substr($forWhomArray[0], 0, 60, 'UTF-8') . "..."; ?>
+                <a href="#"
+                   onclick="showBlock('#<?= $param ?>Block', '#<?= $param ?>Item', '#linkDetail<?= $param ?>'); return false;">детальніше...</a>
+            </li>
+            <div id="<?= $param ?>Block" style="display: none">
+                <?php
+                foreach ($forWhomArray as $key => $item) {
+                    ?>
+                    <li><?php echo $item . ";"; ?></li>
+                    <?php
+                }
+                ?>
+                <a id="linkDetail<?= $param ?>" href="#"
+                   onclick="hideBlock('#<?= $param ?>Block', '#<?= $param ?>Item', '#linkDetail<?= $param ?>');  return false;">
+                    приховати
+                </a>
+            </div>
+            <?php
+        }
+    } ?>
 </ul>
