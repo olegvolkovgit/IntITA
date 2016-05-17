f51db670d0d41dc63e36eec7338fe97ac0935edf
IvannaBezpalko <vnnchkh@gmail.com>
2016-05-17 12:19:59 +0300

    IS-1270

--- protected/models/quiz/PlainTask.php
+++ protected/models/quiz/PlainTask.php
@@ -84,7 +84,7 @@ class PlainTask extends Quiz
 		$criteria->compare('id',$this->id);
 		$criteria->compare('block_element',$this->block_element);
 		$criteria->compare('author',$this->author);
-		$criteria->compare('uid',$this->uid);
+		$criteria->compare('t.uid',$this->uid);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
