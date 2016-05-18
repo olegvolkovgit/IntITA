7265d81c6e4922c841f04b8d4235dae99f96c1f3
Morgunov <morgunov.igor1990@gmail.com>
2016-05-18 20:22:08 +0300

    1

--- protected/modules/_teacher/controllers/_tenant/TenantController.php
+++ protected/modules/_teacher/controllers/_tenant/TenantController.php
@@ -5,3 +5,24 @@
  * Date: 18.05.2016
  * Time: 20:05
  */
+
+
+class TenantController extends TeacherCabinetController
+{
+    public function hasRole(){
+        return Yii::app()->user->model->isTenant();
+    }
+
+    public function actionShowPhrases($id)
+    {
+        $tmp = 1;
+        $tmp2 = 2;
+        $tmp3 = 3;
+
+        $this->renderPartial('/_tenant/_allPhrases', array(
+            'modules' => $tmp,
+            'user' => $tmp2
+        ), false, true);
+    }
+
+}
