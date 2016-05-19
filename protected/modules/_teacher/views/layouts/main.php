07eb5ed409e250dcb5bc07a32e842867194fd9de
Wizlight <Wizlightdragon@gmail.com>
2016-05-19 20:00:02 +0300

    is1468

--- protected/modules/_teacher/views/layouts/main.php
+++ protected/modules/_teacher/views/layouts/main.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="<?php echo StaticFilesHelper::fullPathTo('css', 'regform.css');; ?>"/>
     <link rel="stylesheet" type="text/css" href="<?php echo Config::getBaseUrl(); ?>/css/style.css"/>
     <script src="<?php echo StaticFilesHelper::fullPathTo('angular', 'js/angular.min.js'); ?>"></script>
+    <script src="<?php echo StaticFilesHelper::fullPathTo('js', 'cabinet/authForm.js'); ?>"></script>
     <script async type="text/javascript" src="<?php echo Config::getBaseUrl(); ?>/scripts/trimField.js"></script>
     <title><?php echo Yii::app()->name; ?></title>
 </head>
@@ -17,6 +18,22 @@
     <body>
         <div id="contentBoxMain">
             <?php echo $content; ?>
+            <?php
+            $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
+                'id' => 'forgotpass',
+                'themeUrl' => Config::getBaseUrl() . '/css',
+                'cssFile' => 'jquery-ui.css',
+                'theme' => 'my',
+                'options' => array(
+                    'width' => 540,
+                    'autoOpen' => false,
+                    'modal' => true,
+                    'resizable' => false
+                ),
+            ));
+            $this->renderPartial('/cabinet/_forgotpass');
+            $this->endWidget('zii.widgets.jui.CJuiDialog');
+            ?>
         </div>
     </body>
 </html>
