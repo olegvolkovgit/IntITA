31e4a478166ee606e247ea26dce9947cfad6a21a
IvannaBezpalko <vnnchkh@gmail.com>
2016-05-30 14:09:18 +0300

    Minor fix

--- protected/views/payments/_account.php
+++ protected/views/payments/_account.php
@@ -52,7 +52,7 @@
             <tr>
                 <td>1</td>
                 <td style="text-align: left">Освітні послуги в науково-технічному напрямку - програмування та
-                    комп'ютерна грамотність (<?php echo Invoice::getProductTitle($model); ?>)
+                    комп'ютерна грамотність (<?php echo CHtml::encode(Invoice::getProductTitle($model)); ?>)
                 </td>
                 <td><span
                         id="summa"><?php echo number_format(CommonHelper::getPriceUah($model->summa), 2, ",","&nbsp;"); ?></span>
