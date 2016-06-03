50b728ea1c31a0832300eba4d3d0e78a25f062cd
Wizlight <Wizlightdragon@gmail.com>
2016-06-04 02:20:58 +0300

    edit revision message

--- angular/js/lecture_revision_app/services/sendRevisionMessage.js
+++ angular/js/lecture_revision_app/services/sendRevisionMessage.js
@@ -26,7 +26,7 @@ angular
                             '</div>'+
                             '<div class="form-group col-md-12">'+
                             '<label>Лист</label>'+
-                            '<textarea class="form-control" style="resize: none" rows="6" id="messageText" required><a href="'+response.data.link+'" target="_blank">Посилання на ревізію</a></textarea>'+
+                            '<textarea class="form-control" style="resize: none" rows="6" id="messageText" required>Посилання на ревізію: '+response.data.link+'</textarea>'+
                             '</div>'+
                             '</form>'+
                             '</div>'+
