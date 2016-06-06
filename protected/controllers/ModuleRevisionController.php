c1cd2428d0d49e1ada8e88f461fb5aeb3303e916
Wizlight <Wizlightdragon@gmail.com>
2016-06-07 01:08:09 +0300

    add build module tree

--- protected/controllers/ModuleRevisionController.php
+++ protected/controllers/ModuleRevisionController.php
@@ -53,8 +53,72 @@ class ModuleRevisionController extends Controller {
         $idCourse = Yii::app()->request->getPost('idCourse');
         $moduleRev = RevisionModule::model()->findAll();
         $relatedTree = RevisionModule::getModulesTree($idCourse);
-        $json = $this->buildLectureTreeJson($moduleRev, $relatedTree);
+        $json = $this->buildModuleTreeJson($moduleRev, $relatedTree);
 
         echo $json;
+    }
+
+    private function buildModuleTreeJson($modules, $moduleTree) {
+        $jsonArray = [];
+        foreach ($modules as $module) {
+            $node = array();
+            $node['text'] = "Ревізія №" . $module->id_module_revision . " " .
+                $module->properties->title_ua . ". Статус: <strong>" . $module->getStatus().'</strong>'.
+                ' Створена: '.$module->properties->start_date.' Модифікована: '.$module->properties->update_date;
+            $node['selectable'] = false;
+            $node['id'] = $module->id_module_revision;
+            $node['creatorId'] = $module->properties->id_user_created;
+            $node['isSendable'] = $module->isSendable();
+            $node['isApprovable'] = $module->isApprovable();
+            $node['isCancellable'] = $module->isCancellable();
+            $node['isEditable'] = $module->isEditable();
+            $node['isRejectable'] = $module->isRejectable();
+            $node['isSendedCancellable'] = $module->isRevokeable();
+            $node['isReadable'] = $module->isReleaseable();
+            $node['isEditCancellable'] = $module->isEditable();
+            $node['canRestoreEdit'] = $module->isCancelledEditor();
+
+            $this->appendNode($jsonArray, $node, $moduleTree);
+        }
+        return json_encode(array_values($jsonArray));
+    }
+
+    /**
+     * Function to build tree of lectures based on quickUnion data structure
+     * @param $tree - tree to build, passed by reference
+     * @param $node - node to add
+     * @param $parents - quik union structre
+     */
+    private function appendNode(&$tree, $node, $parents) {
+        if ($parents[$node['id']] == $node['id']) {
+            //if root node
+            $tree[$node['id']] = $node;
+        } else {
+            $path = [];
+            $parentId = $parents[$node['id']];
+
+            //building path from root to target node
+            array_push($path, $parentId);
+            while ($parents[$parentId] != $parentId) {
+                array_push($path, $parents[$parentId]);
+                $parentId = $parents[$parentId];
+            }
+
+            //finding reference to target node
+            $targetNode = &$tree;
+            while (count($path) != 0) {
+                if (!array_key_exists('nodes', $targetNode)) {
+                    $targetNode =& $targetNode[array_pop($path)];
+                } else {
+                    $targetNode =& $targetNode['nodes'][array_pop($path)];
+                }
+            }
+
+            //adding node to 'nodes' array in target node
+            if (!array_key_exists('nodes', $targetNode)) {
+                $targetNode['nodes'] = array();
+            }
+            $targetNode['nodes'][$node['id']] = $node;
+        }
     }
 }
