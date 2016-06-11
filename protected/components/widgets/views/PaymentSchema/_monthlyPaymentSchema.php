36ec73bd9e6d28f5f54cdbb38af09af55f8b120d
IvannaBezpalko <vnnchkh@gmail.com>
2016-06-11 13:30:45 +0300

    IS-1549, IS-1504

--- protected/components/widgets/views/PaymentSchema/_monthlyPaymentSchema.php
+++ protected/components/widgets/views/PaymentSchema/_monthlyPaymentSchema.php
@@ -32,12 +32,11 @@
                                 <div class="numbers">
                                     <span><?php
                                         //todo
-                                        echo round($schema->getSumma($model)/12) . ' ' . Yii::t('courses', '0322') . '/' .
-                                            Yii::t('payments', '0865') . ' х 12 ' . Yii::t('course', '0323') . ' = '; ?>
-                                        <b>
-                                            <?php echo round(($educForm == 'online')?$schema->getSumma($model):
-                                            $schema->getSummaOffline($model)). ' ' .
-                                                Yii::t('courses', '0322') ?>
+                                        echo Yii::t('courses', '0322') . sprintf ("%01.2f", round($schema->getSumma($model)/12, 2)) . '/' .
+                                            Yii::t('payments', '0865') . ' х 12 ' . Yii::t('course', '0323'); ?>
+                                        &asymp;<b>
+                                            <?php echo Yii::t('courses', '0322') . sprintf ("%01.2f", round(($educForm == 'online')?$schema->getSumma($model):
+                                            $schema->getSummaOffline($model), 2)) ?>
                                         </b>
                                     </span>
                                 </div>
