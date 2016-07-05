8488237f76235201a2bf23fc8e18d909cf6b95c9
Wizlight <Wizlightdragon@gmail.com>
2016-07-05 19:44:28 +0300

    is 1705

--- protected/views/revision/_addLessonForm.php
+++ protected/views/revision/_addLessonForm.php
@@ -7,10 +7,10 @@
            oninvalid="validateComments(this,'<?php echo Yii::t('validation', '0684'); ?>')"
            oninput="validateComments(this,'<?php echo Yii::t('validation', '0684'); ?>')" >
     <div>Назва (RU):</div>
-    <input class="form-control" type="text" name="titleRu" id="titleRu" pattern="<?php echo Yii::app()->params['titleRUPattern'] ?>+$" maxlength="255"
+    <input class="form-control" type="text" name="titleRu" id="titleRu" ng-model="titleRu" pattern="<?php echo Yii::app()->params['titleRUPattern'] ?>+$" maxlength="255"
            size="70" oninput="validateComments(this,'<?php echo Yii::t('validation', '0685'); ?>')">
     <div>Назва (EN):</div>
-    <input class="form-control" type="text" name="titleEn" id="titleEn" pattern="<?php echo Yii::app()->params['titleENPattern'] ?>+$" maxlength="255" size="70"
+    <input class="form-control" type="text" name="titleEn" id="titleEn" ng-model="titleEn" pattern="<?php echo Yii::app()->params['titleENPattern'] ?>+$" maxlength="255" size="70"
            oninput="validateComments(this,'<?php echo Yii::t('validation', '0685'); ?>')">
     <br>
     <input type="submit" class="btn btn-default" value=<?php echo Yii::t('module', '0383'); ?> onclick="trimLectureName()" ng-disabled=addLesson.$invalid >
