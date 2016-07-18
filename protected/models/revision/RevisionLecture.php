cc597404bbbf9739e1533735ca70deb01e78bd74
RidgeA <antongriadchenko@gmail.com>
2016-07-18 21:54:40 +0300

    Revision filter.

--- protected/models/revision/RevisionLecture.php
+++ protected/models/revision/RevisionLecture.php
@@ -864,16 +864,15 @@ class RevisionLecture extends CRevisionUnitActiveRecord {
     /**
      * revisions id list after filtered
      */
-    public static function getFilteredIdRevisions($status, $idModule,$idAuthor=null) {
-        $sqlCancelledEditor = '(vcp.id_user_cancelled_edit IS NOT NULL)';
-        $sqlCancelled = '(vcp.id_user_cancelled IS NOT NULL and vcp.id_user_released IS NULL)';
-        $sqlReady = '(vcp.id_user_released IS NOT NULL and vcp.id_user_cancelled IS NULL)';
-        $sqlApproved = '(vcp.id_user_approved IS NOT NULL and vcp.id_user_released IS NULL and vcp.id_user_cancelled IS NULL and vcp.id_user_cancelled_edit IS NULL 
-        and vcp.id_user_proposed_to_release IS NULL)';
-        $sqlRejected = '(vcp.id_user_rejected IS NOT NULL)';
-        $sqlSent = '(vcp.id_user_sended_approval IS NOT NULL and vcp.id_user_rejected IS NULL and vcp.id_user_approved IS NULL)';
-        $sqlEditable = '(vcp.id_user_sended_approval IS NULL and vcp.id_user_approved IS NULL and vcp.id_user_cancelled_edit IS NULL and vcp.id_user_cancelled IS NULL and vcp.id_user_released IS NULL)';
-        $sqlProposedToRelease = '(vcp.id_user_proposed_to_release IS NOT NULL and (vcp.proposed_to_release_date>vcp.release_date or vcp.release_date IS NULL))';
+    public static function getFilteredIdRevisions($status, $idModule, $idAuthor = null) {
+        $sqlCancelledEditor = "(vcp.id_state = " . RevisionState::CancelledAuthorState . ")";
+        $sqlCancelled = "(vcp.id_state = " . RevisionState::CancelledState . ")";
+        $sqlReady = "(vcp.id_state = " . RevisionState::ReleasedState . ")";
+        $sqlApproved = "(vcp.id_state = " . RevisionState::ApprovedState . ")";
+        $sqlRejected = "(vcp.id_state = " . RevisionState::RejectedState . ")";
+        $sqlSent = "(vcp.id_state = " . RevisionState::SendForApprovalState . ")";
+        $sqlEditable = "(vcp.id_state = " . RevisionState::EditableState . ")";
+        $sqlProposedToRelease = "(vcp.id_state = " . RevisionState::ReadyForRelease . ")";
 
         $finalSql = '';
         $authorSql = '';
