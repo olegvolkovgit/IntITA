c9c97e5264b4051b21a73454ccd866ed583d6f64
Nick Zorych <neo.vsmu@gmail.com>
2016-08-10 21:19:06 +0300

    hotfix authorization

--- angular/js/teacher/app.js
+++ angular/js/teacher/app.js
@@ -2,5 +2,6 @@
  * Created by Quicks on 15.12.2015.
  */
 angular
-    .module('teacherApp',['datatables','ui.bootstrap','ngBootbox','messagesRouter','sharedLinkstRouter','responseRouter','graduatesRouter','cabinetRouter', 'adminRouter','modulesRouter','authorRouter','consultantRouter','teacherConsultantRouter','trainerRouter','studentRouter','tenantRouter','accountantRouter','contentManagerRouter','directive.loading'])
+    .module('teacherApp',['datatables','ui.bootstrap','ngBootbox','messagesRouter','cabinetRouter', 'adminRouter','modulesRouter','authorRouter','consultantRouter','teacherConsultantRouter','trainerRouter','studentRouter','tenantRouter',
+        'accountantRouter','contentManagerRouter','directive.loading','graduatesRouter', 'sharedLinksRouter', 'responseRouter'])
     ;
--- angular/js/teacher/routers/sharedLinksRouter.js
+++ angular/js/teacher/routers/sharedLinksRouter.js
@@ -2,31 +2,31 @@
  * Created by adm on 10.08.2016.
  */
 angular
-    .module('sharedLinkstRouter',['ui.router']).
+    .module('sharedLinksRouter',['ui.router']).
 config(function ($stateProvider) {
     $stateProvider
         .state('sharedlinks', {
             url: '/sharedlinks',
             cache: false,
-            templateUrl: basePath+"/_teacher/_admin/shareLink/index"
+            templateUrl: "/_teacher/_admin/shareLink/index"
         })
         .state('sharedlinks/detail/:linkId', {
             url: '/sharedlinks/detail/:linkId',
             cache: false,
             templateUrl: function($stateParams){
-                return basePath+'/_teacher/_admin/shareLink/view/id/'+$stateParams.linkId
+                return '/_teacher/_admin/shareLink/view/id/'+$stateParams.linkId
             }
         })
         .state('sharedlinks/create', {
             url: '/sharedlinks/create',
             cache: false,
-            templateUrl: basePath+"/_teacher/_admin/shareLink/create"
+            templateUrl: "/_teacher/_admin/shareLink/create"
         })
         .state('sharedlinks/edit/:linkId', {
             url: '/sharedlinks/edit/:linkId',
             cache: false,
             templateUrl: function($stateParams){
-                return basePath+'/_teacher/_admin/shareLink/update/id/'+$stateParams.linkId
+                return '/_teacher/_admin/shareLink/update/id/'+$stateParams.linkId
             }
         })
-})
+});
