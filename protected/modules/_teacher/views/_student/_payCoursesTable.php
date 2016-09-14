914a1aaa0407d9bde38db85b3f50618b7531d581
Nick Zorych <neo.vsmu@gmail.com>
2016-09-14 19:54:56 +0300

    hotfix

--- protected/modules/_teacher/views/_student/_payCoursesTable.php
+++ protected/modules/_teacher/views/_student/_payCoursesTable.php
@@ -4,7 +4,7 @@
             <div class="dataTable_wrapper">
                 <table ng-table="paidCoursesTable" class="table table-striped table-bordered table-hover" id="agreementsTable">
                     <tr ng-repeat="row in $data">
-                        <td data-title="'Назва'"><div ng-if="!row.course.cancelled"><a href="course/{{row.course.language}}/{{row.course.alias}}">{{row.course.title_ua}}</a></div>
+                        <td data-title="'Назва'"><div ng-if="!row.course.cancelled"><a href="/course/{{row.course.language}}/{{row.course.alias}}">{{row.course.title_ua}}</a></div>
                             <div ng-if="row.course.cancelled">{{row.course.title_ua}} (скасований)</div>
                         </td>
                         <td data-title="'Сума, грн'"><div ng-if="row.course.course_price">{{row.course.course_price *usd}}</div><div ng-if="!row.course.course_price">бескоштовно</div></td>
--- protected/modules/_teacher/views/_student/_payModulesTable.php
+++ protected/modules/_teacher/views/_student/_payModulesTable.php
@@ -4,7 +4,7 @@
             <div class="dataTable_wrapper">
                 <table ng-table="paidModuesTable" class="table table-striped table-bordered table-hover" id="agreementsTable">
                     <tr ng-repeat="row in $data">
-                        <td data-title="'Назва'"><div ng-if="!row.module.cancelled"><a href="module/{{row.module.language}}/{{row.id_module}}">{{row.module.title_ua}}</a></div>
+                        <td data-title="'Назва'"><div ng-if="!row.module.cancelled"><a href="/module/{{row.module.language}}/{{row.id_module}}">{{row.module.title_ua}}</a></div>
                                                  <div ng-if="row.module.cancelled">{{row.module.title_ua}} (скасований)</div>
                         </td>
                         <td data-title="'Сума, грн'"><div ng-if="row.module.module_price">{{row.module.module_price *usd}}</div><div ng-if="!row.module.module_price">бескоштовно</div></td>
