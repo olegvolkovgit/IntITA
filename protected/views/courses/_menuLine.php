b262b2913a3978027cd5a635343eb87062f2c150
Wizlight <Wizlightdragon@gmail.com>
2016-09-16 15:05:48 +0300

    slider and courses fix

--- protected/views/courses/_menuLine.php
+++ protected/views/courses/_menuLine.php
@@ -8,31 +8,38 @@
     <h1><?php echo Yii::t('courses', '0066'); ?></h1>
 
     <div class="coursesType">
-        <div class='selectType sourse <?php if ($select == 'junior') echo 'selectedSelector' ?>'><a
-                href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'junior')); ?>">
-                <?php echo Yii::t('courses', '0140'); ?></a>&nbsp;<span
-                class='courseNum'><?php echo $counters["junior"]; ?></span>
+        <div class="category">
+            <div class='selectType sourse <?php if ($select == 'junior') echo 'selectedSelector' ?>'><a
+                    href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'junior')); ?>">
+                    <?php echo Yii::t('courses', '0140'); ?></a>&nbsp;<span
+                    class='courseNum'><?php echo $counters["junior"]; ?></span>
+            </div>
+            <div class='selectLine sourse'>&nbsp;&nbsp;<img
+                    src="<?php echo StaticFilesHelper::createPath('image', 'courses', 'coursesline2.png'); ?>"/>&nbsp;&nbsp;
+            </div>
         </div>
-        <div class='selectLine sourse'>&nbsp;&nbsp;<img
-                src="<?php echo StaticFilesHelper::createPath('image', 'courses', 'coursesline2.png'); ?>"/>&nbsp;&nbsp;
-        </div>
-        <div class='selectType sourse <?php if ($select == 'middle') echo 'selectedSelector' ?>'><a
-                href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'middle')); ?>">
-                <?php echo Yii::t('courses', '0141'); ?></a>&nbsp;<span
-                class='courseNum'><?php echo $counters["middle"]; ?></span>
-        </div>
-        <div class='selectLine sourse'>&nbsp;&nbsp;<img
-                src="<?php echo StaticFilesHelper::createPath('image', 'courses', 'coursesline2.png'); ?>"/>&nbsp;&nbsp;
+<div class="category">
+    <div class='selectType sourse <?php if ($select == 'middle') echo 'selectedSelector' ?>'><a
+            href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'middle')); ?>">
+            <?php echo Yii::t('courses', '0141'); ?></a>&nbsp;<span
+            class='courseNum'><?php echo $counters["middle"]; ?></span>
+    </div>
+    <div class='selectLine sourse'>&nbsp;&nbsp;<img
+            src="<?php echo StaticFilesHelper::createPath('image', 'courses', 'coursesline2.png'); ?>"/>&nbsp;&nbsp;
+    </div>
+</div>
+
+        <div class="category">
+            <div class='selectType sourse <?php if ($select == 'senior') echo 'selectedSelector' ?>'><a
+                    href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'senior')); ?>">
+                    <?php echo Yii::t('courses', '0142'); ?></a>&nbsp;<span
+                    class='courseNum'><?php echo $counters["senior"]; ?></span>
+            </div>
+            <div class='selectLine sourse'>&nbsp;&nbsp;<img
+                    src="<?php echo StaticFilesHelper::createPath('image', 'courses', 'coursesline2.png'); ?>"/>&nbsp;&nbsp;
+            </div>
         </div>
 
-        <div class='selectType sourse <?php if ($select == 'senior') echo 'selectedSelector' ?>'><a
-                href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'senior')); ?>">
-                <?php echo Yii::t('courses', '0142'); ?></a>&nbsp;<span
-                class='courseNum'><?php echo $counters["senior"]; ?></span>
-        </div>
-        <div class='selectLine sourse'>&nbsp;&nbsp;<img
-                src="<?php echo StaticFilesHelper::createPath('image', 'courses', 'coursesline2.png'); ?>"/>&nbsp;&nbsp;
-        </div>
         <div class='sourse <?php if ($select == 'all') echo 'selectedSelector' ?>'><a
                 href="<?php echo Yii::app()->createUrl('courses/index', array('selector' => 'all')); ?>">
                 <?php echo Yii::t('courses', '0143'); ?></a>&nbsp;<span
--- protected/views/layouts/column1.php
+++ protected/views/layouts/column1.php
@@ -1,7 +1,8 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
 
-<div id="content">
+<div id="content"
+class="<?php if (Yii::app()->controller->id == 'site' && (Yii::app()->controller->action->id == 'index') || (Yii::app()->controller->id == 'aboutus')) echo 'sliderPage' ?>">
 	
 	<?php echo $content;?>
 
