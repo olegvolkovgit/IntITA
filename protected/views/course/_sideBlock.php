9a181232f7616eeb2c8fb5797a374906867b0afb
Wizlight <Wizlightdragon@gmail.com>
2016-09-20 14:33:44 +0300

    add responsive lecture page is1928 1934

--- protected/views/course/_sideBlock.php
+++ protected/views/course/_sideBlock.php
@@ -9,11 +9,17 @@
     <?php if ($model->$param != '') { ?>
         <p class="subCourseInfo"><b><?=$model->getPropertyLabel($param);?></b></p>
         <?php
+        $forWhomArray = explode(";", $model->$param);
         if(strlen($model->$param) < 75){?>
-            <li><?php echo $model->$param; ?></li>
+            <?php
+            foreach ($forWhomArray as $key => $item) {
+                ?>
+                <li><?php echo $item . ";"; ?></li>
+                <?php
+            }
+            ?>
         <?php }
         else {
-            $forWhomArray = explode(";", $model->$param);
             ?>
             <li id="<?= $param ?>Item"><?= (strlen($forWhomArray[0]) <= 60) ? $forWhomArray[0] : mb_substr($forWhomArray[0], 0, 60, 'UTF-8') . "..."; ?>
                 <a class="hideBlockLink" href=""
