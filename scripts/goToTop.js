4c32f44a72f9fa2fadb96703baf804950bbed78e
Wizlight <Wizlightdragon@gmail.com>
2016-09-23 13:32:19 +0300

    responsive goTop fix responsive footer

--- scripts/goToTop.js
+++ scripts/goToTop.js
@@ -11,6 +11,9 @@ function rocketMove(element,w) {
     setTimeout(function () {
         $('#exhaust').hide();
     }, 500);
+    setTimeout(function () {
+        $('#rocket').hide();
+    }, 3000);
 }
 
 function goUp(){
@@ -18,11 +21,10 @@ function goUp(){
     var wPosR=$(document).outerWidth()/2-$(window).width()*0.225;
     var hPosE=$(document).outerHeight()-$('#exhaust').height();
     var wPosE=$(document).outerWidth()/2-$(window).width()*0.4167;
+    $('#rocket').show();
     $('#rocket').offset({top:hPosR, left:wPosR});
     $('#exhaust').show();
     $('#exhaust').offset({top:hPosE, left:wPosE});
-
-    $('#rocket').show();
 
     $('body,html').animate({scrollTop: 0}, 2700);
     rocketMove($('#rocket'),wPosR);
