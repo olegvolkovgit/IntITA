bb258937cc541264bb8443e3a232cc88a126dcff
Wizlight <Wizlightdragon@gmail.com>
2016-09-24 11:48:04 +0300

    fix hamburger

--- scripts/hamburgermenu.js
+++ scripts/hamburgermenu.js
@@ -12,15 +12,6 @@ $(document).ready(function(){
         }
         $("#hambNav").css({display: "block"});
     }
-
-    if ($(window).width() <= '800') {
-        $('#share42').css({"margin-left":"20px"});
-        $('.share42-item').css({"height":"28px","width":"36px","margin-bottom":"16px"});
-        $('.share42-item a').css({"height":"28px","width":"36px"});
-        $('.share42-item a').css({"background-image":"url(/scripts/share42/iconsMini.png)"});
-        $('#sharing .share42-counter').css({"width":"34px"});
-        $('#sharingMain .share42-counter').css({"width":"34px"});
-    }
 });
 
 $(window).scroll(function() {
