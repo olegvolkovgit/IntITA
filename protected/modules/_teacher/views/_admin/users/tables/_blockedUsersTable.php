620a1daf40ccc9c2d1fcd8080de028c93b706f90
Nick Zorych <neo.vsmu@gmail.com>
2016-10-17 22:44:38 +0300

    add filters and sorting

--- protected/modules/_teacher/views/_admin/users/tables/_blockedUsersTable.php
+++ protected/modules/_teacher/views/_admin/users/tables/_blockedUsersTable.php
@@ -1,20 +1,18 @@
 <div class="col-lg-12">
-    <br>
-    <br>
-    <br>
+
     <div class="panel panel-default">
         <div class="panel-body">
             <div class="dataTable_wrapper">
                 <table ng-table="blockedUsersTable" class="table table-bordered table-striped table-condensed">
                     <tr ng-repeat="row in $data track by $index">
-                        <td data-title="'ПІБ'">
+                        <td data-title="'ПІБ'" sortable="'registeredUser.fullName'" filter="{'registeredUser.fullName': 'text'}">
                             <a ng-href="#/admin/users/user/{{row.registeredUser.id}}">{{row.registeredUser.firstName}} {{row.registeredUser.middleName}} {{row.registeredUser.secondName}}</a>
                         </td>
-                        <td data-title="'Email'">
+                        <td data-title="'Email'" sortable="'registeredUser.email'" filter="{'registeredUser.email': 'text'}">
                             <a ng-href="#/admin/users/user/{{row.registeredUser.id}}">{{row.registeredUser.email}}</a>
                         </td>
-                        <td data-title="'Дата блокування'">{{row.locked_date | shortDate:"dd-MM-yyyy HH:mm:ss "  }}</td>
-                        <td data-title="'Заблоковано користувачем'"><span ng-if="row.lockedBy">{{row.lockedBy.firstName}} {{row.lockedBy.middleName}} {{row.lockedBy.secondName}}</span>
+                        <td data-title="'Дата блокування'" sortable="'locked_date'">{{row.locked_date | shortDate:"dd-MM-yyyy HH:mm:ss "  }}</td>
+                        <td data-title="'Заблоковано користувачем'" filter="{'lockedBy.fullName': 'text'}" sortable="'lockedBy.firstName'" ><span ng-if="row.lockedBy">{{row.lockedBy.firstName}} {{row.lockedBy.middleName}} {{row.lockedBy.secondName}}</span>
                                                                     <span ng-if="!row.lockedBy">Administrator</span>
                         </td>
                         <td data-title="'Профіль'"><a ng-href="/profile/{{row.registeredUser.id}}" target="_blank">Профіль</a></td>
