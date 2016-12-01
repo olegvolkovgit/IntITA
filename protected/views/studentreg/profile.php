c7dad9c89e8d5a852bf00f7cade3495d44b6eae2
Wizlight <Wizlightdragon@gmail.com>
2016-12-01 13:25:55 +0200

    added  functionality from consultant role to teacherConsultant role

--- protected/views/studentreg/profile.php
+++ protected/views/studentreg/profile.php
@@ -183,7 +183,6 @@ $this->breadcrumbs = array(Yii::t('breadcrumbs', '0054'));
 <script
     src="<?php echo StaticFilesHelper::fullPathTo('css', 'bower_components/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
 <script src="<?php echo StaticFilesHelper::fullPathTo('angular', 'js/bootbox.min.js'); ?>"></script>
-<script src="<?php echo StaticFilesHelper::fullPathTo('js', 'profileDialogs.js'); ?>"></script>
 <link
     href="<?php echo StaticFilesHelper::fullPathTo('css', 'bower_components/bootstrap/dist/css/bootstrap.min.css'); ?>"
     rel="stylesheet">
