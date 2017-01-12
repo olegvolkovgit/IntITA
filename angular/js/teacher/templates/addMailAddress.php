a459f364528cd2f4d593cc252dc2e16b17ec80fe
Nick Zorych <neo.vsmu@gmail.com>
2017-01-12 17:13:42 +0200

    small fixes

---
+++ angular/js/teacher/templates/addMailAddress.php
@@ -0,0 +1,10 @@
+<div class="panel panel-default" ng-controller="mailTemplatesCtrl">
+    <div class="panel-body" >
+        <div class="col-xs-2">
+            <input class="form-control" id="ex1" type="text">
+        </div>
+        <div class="col-xs-3">
+            @<?php echo 'test' ?>
+        </div>
+    </div>
+</div>
