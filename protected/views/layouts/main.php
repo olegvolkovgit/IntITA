37a94549b78525ec75f1699de2b9a90486a71fa6
Wizlight <Wizlightdragon@gmail.com>
2017-02-01 18:27:27 +0200

    chat ypt

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -538,7 +538,7 @@ $header = new Header();
     <div ng-controller="chat-controller as main" class="dnd-container">
         <div ng-show="init" class="draggable chat mini ng-class:{mini: state==1, full: state==2}" ng-click="" >
             <div class="logo ignore"></div>
-            <iframe style="width: 100%;height: 100%;border: none;" src="https://localhost:8080/crmChat"></iframe>
+            <iframe style="width: 100%;height: 100%;border: none;" src="https://qa.intita.com/crmChat"></iframe>
             <div class="window_panel ignore" style="">
                 <div id="minimize_btn" class="material-icons" ng-click="minimizete()">indeterminate_check_box</div>
                 <div id="fullscreen_btn" class="material-icons" ng-click="fullScreen()">web_asset</div>
