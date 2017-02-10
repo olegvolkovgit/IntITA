1de1d76367013e24c7d49e4b03981cc4300c272b
Wizlight <Wizlightdragon@gmail.com>
2017-02-10 22:43:22 +0200

    add chat to main app and teacher app

--- angular/js/app.js
+++ angular/js/app.js
@@ -2,6 +2,4 @@
 
 /* App Module */
 angular
-    .module('lessonApp', ['ui.bootstrap', 'ngRoute','ipCookie']);
-angular
-    .module('mainApp', ['mainApp.directives','ui.bootstrap','oi.select','ngResource','paymentsSchemes.directives', 'ngSanitize', 'ui.select','IntITAMessenger']);
+    .module('mainApp', ['mainApp.directives','ui.bootstrap','oi.select','ngResource','paymentsSchemes.directives', 'ngSanitize', 'ui.select','chatIntITAMessenger']);
