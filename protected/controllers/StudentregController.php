c49f0ca0ca459c2528f10f0810baf6343f7f4286
wizlight <Wizlightdragon@gmail.com>
2017-07-22 15:34:47 +0300

    fix

--- protected/controllers/StudentregController.php
+++ protected/controllers/StudentregController.php
@@ -225,7 +225,7 @@ class StudentRegController extends Controller
             }
 
             $model->update(array('firstName','secondName','nickname','phone','address','education','educform','interests','aboutUs','aboutMy','facebook','googleplus',
-                'linkedin','twitter','skype','passport','document_type','inn','passport_issued','prev_job','current_job','education_shift'));
+                'linkedin','twitter','skype','prev_job','current_job','education_shift'));
             $model->updateUserCareer($careers);
             $model->updateUserSpecialization($specializations);
 
@@ -407,8 +407,7 @@ class StudentRegController extends Controller
                                'birthday', 'email', 'facebook', 'googleplus', 'linkedin',
                                 'twitter', 'phone', 'address', 'education',
                                 'interests', 'aboutUs', 'aboutMy', 'avatar',
-                                'skype', 'country', 'city', 'prev_job', 'passport',
-                                'document_issued_date', 'inn', 'passport_issued', 'current_job'];
+                                'skype', 'country', 'city', 'prev_job', 'current_job'];
 
         foreach ($model->attributes as $key => $attribute){
             if(in_array($key, $student_attributes)) {
