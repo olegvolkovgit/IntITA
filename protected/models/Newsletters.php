c58a73b29a819e0bc4926c90e49b5e6171e20d0f
NIck Zorych <neo.vsmu@gmail.com>
2017-08-18 13:57:27 +0300

    fix email by category

--- protected/models/Newsletters.php
+++ protected/models/Newsletters.php
@@ -284,6 +284,9 @@ class Newsletters extends CActiveRecord implements ITask
                 if ($category){
                     array_push($result,['id'=>$category->id, 'name'=>$category->title]);
                 }
+                else{
+                    array_push($result,['id'=>0, 'name'=>'Вся база email']);
+                }
                 break;
             }
             default:{
