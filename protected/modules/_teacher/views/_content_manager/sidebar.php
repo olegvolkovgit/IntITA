c2c5e6443d419ae37667141f5e856b86aae854ad
Ivan Andrienko <wdprogr@gmail.com>
2017-09-18 16:32:39 +0300

    created tables in trainer->student (_career)

--- protected/modules/_teacher/views/_content_manager/sidebar.php
+++ protected/modules/_teacher/views/_content_manager/sidebar.php
@@ -1,5 +1,8 @@
 <li>
-    <a href="#/content_manager">
-        <i class="fa fa-bar-chart-o fa-fw"></i>Контент менеджер
+    <a href="#/content_manager" class="show_elem">
+        <i class="fa fa-folder-open-o fa-fw"></i> Контент менеджер
+    </a>
+    <a href="#/content_manager" uib-tooltip="Контент менеджер" tooltip-placement="right" class="hid" style="display: none">
+        <i class="fa fa-folder-open-o fa-fw"></i>
     </a>
 </li>
