dd5bc4726a087d65134374b69b95e00145b65a16
wizlight <wizlightdragon@gmail.com>
2017-10-11 18:55:35 +0300

    crm upd

--- protected/models/crm/tasksState/TaskState.php
+++ protected/models/crm/tasksState/TaskState.php
@@ -70,7 +70,8 @@ abstract class TaskState {
                 'order' => 'id desc',
             ));
         if($lastHistoryTask && $lastHistoryTask->id_task!=$this->idTask){
-            $lastHistoryTask->idTask->state->changeTo('Paused', Yii::app()->user);
+            if($lastHistoryTask->id_state!=CrmTaskStatus::PAUSED)
+                $lastHistoryTask->idTask->state->changeTo('Paused', Yii::app()->user);
         }
 
         $this->_changeState($this->idTask, self::Executed, $user->getId());
