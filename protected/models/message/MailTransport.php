22d446db6e28caad7787cc81d6b4bb185cb5e27a
Nick Zorych <neo.vsmu@gmail.com>
2017-11-07 23:19:08 +0200

    fix email headers

--- protected/models/message/MailTransport.php
+++ protected/models/message/MailTransport.php
@@ -7,23 +7,20 @@ class MailTransport implements IMailSender{
 
     public function send($mailto, $nameFrom, $subject, $text)
     {
-        if (!$nameFrom)
-            $nameFrom = Config::getBaseUrlWithoutSchema();
-        $headers = "From: " . Yii::app()->name . "@" . $nameFrom . "\n"
-            . "MIME-Version: 1.0\n"
-            . "Content-Type: text/html;charset=\"utf-8\"" . "\n";
-        if ($this->template != '') {
-            $text = $this->template;
-        }
-        $this->viewPath = ($dir = Yii::getPathOfAlias($this->viewPath)) ? $dir : Yii::app()->viewPath . DIRECTORY_SEPARATOR . 'mail';
+        $mailPath = Config::getNotifyEmail();
+        $headers = "From: IntITA <{$mailPath}>". "\r\n"
+            . "MIME-Version: 1.0". "\r\n"
+            . "Reply-To: {$mailPath}" . "\r\n"
+            . "Return-Path: {$mailPath}". "\r\n"
+            . "Content-type: text/html;charset=utf-8" . "\r\n";
+
         $message = Yii::app()->controller->renderFile($this->viewPath . DIRECTORY_SEPARATOR . 'mailLayout.php', array(
             'content' => $text,
             'userEmail' => $mailto
         ), true);
-
         $message = $message . "\n";
 
-        return mail($mailto, $subject, $message, $headers);
+        return mail($mailto, mb_encode_mimeheader($subject,"UTF-8"), $message, $headers, "-f {$mailPath}");
     }
 
     public function renderBodyTemplate($template, $params){
