9d1ba9cefe853b73ca06077562f86e5d00f83186
Nick Zorych <neo.vsmu@gmail.com>
2017-11-25 12:43:00 +0200

    progress

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -54,6 +54,7 @@ $header = new Header();
     <script src="<?php echo StaticFilesHelper::fullPathTo('angular', 'js/main_app/services/transformRequest.js'); ?>"></script>
     <link rel='stylesheet' href="<?php echo StaticFilesHelper::fullPathTo('angular', 'js/loading-bar.min.css'); ?>" type='text/css' media='all' />
     <script src="<?php echo StaticFilesHelper::fullPathTo('angular', 'js/loading-bar.min.js'); ?>"></script>
+    <script src="<?php echo StaticFilesHelper::fullPathTo('angular', 'js/ngBootbox.min.js'); ?>"></script>
 
     <?php if (!Yii::app()->user->isGuest) { ?>
         <script src="<?php echo Config::getFullChatPath()."/js/ITA.js" ?>"></script>
--- protected/views/studentreg/_projects.php
+++ protected/views/studentreg/_projects.php
@@ -6,22 +6,24 @@
  * Time: 12:36
  */
 ?>
-<div class="col-md-12 col-sm-12">
-    <button class="btn btn-primary">Додати проект</button>
+<div class="col-md-12 col-sm-12" >
+    <button class="btn btn-primary" ng-click="addProject()"> Додати проект</button>
 </div>
-<div class="row">
+
+<div class="row" ng-repeat="project in projects">
         <div class="col-md-2 col-sm-2">
                 <strong>Проект:</strong>
         </div>
         <div class="col-md-5 col-sm-5">
-            <a href="{project.url}" target="_blank">{project.title}</a>
+            <a href="{{project.url}}" target="_blank">{{project.title}}</a>
         </div>
         <div class="col-md-5 col-sm-5">
             <span class="col-sm-4">
-            <button class="btn btn-sm btn-primary">Змінити</button>
+            <button class="btn btn-sm btn-primary" ng-click="editProject(project.id)">Змінити</button>
             </span>
             <span class="col-sm-1">
-            <button class="btn btn-sm btn-success">Запит на перевірку</button>
+            <button class="btn btn-sm btn-success" ng-click="makeApproveRequest(project.id)">Запит на перевірку</button>
             </span>
         </div>
+
 </div>
