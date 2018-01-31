5355ad638838f99a77c831d056d28b4e006edee6
Viacheslav <viacheslavolshevskyi@gmail.com>
2018-01-31 17:26:47 +0200

    Task #4340 resolved v 2

--- scripts/hideBlock.js
+++ scripts/hideBlock.js
@@ -1,22 +1,51 @@
 /**
  * Created by Ivanna on 08.05.2015.
- */
+ * */
+var date = new Date();
+date.setMonth(date.getMonth()+1);
+
+$('#enter_button').click(function () {
+    document.cookie = "displayBlockTeachers=true;expires="+date;
+});
+
+if(!getCookie('displayBlockTeachers')){
+    document.cookie = "displayBlockTeachers=true;expires="+date;
+}
+
 function getCookie(name) {
     var matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
     ));
-        return matches ? decodeURIComponent(matches[1]) : undefined;
-    }
+        return matches ? decodeURIComponent(matches[1]) : false;
+}
+
 function checkShowingBlockInTeachers() {
     var blockStatus = getCookie('displayBlockTeachers');
     if (blockStatus == 'false') {
-        $('.teacherForm').hide();
+        $(".teacherForm").hide();
+    }
+    else if(blockStatus=='true' && $(document).width()>800){
+        $(".two .teacherForm").show();
+        $(".one .teacherForm").hide();
+    }
+    if(blockStatus=='true' && $(document).width()<=800){
+        $(".one .teacherForm").show();
+        $(".two .teacherForm").hide();
     }
 }
+
 checkShowingBlockInTeachers();
+
+window.addEventListener("resize", function() {
+    checkShowingBlockInTeachers();
+}, false);
+window.addEventListener("orientationchange", function() {
+    checkShowingBlockInTeachers();
+}, false);
+
 
 function xexx()
 {
     $('.teacherForm').hide();
-    document.cookie = "displayBlockTeachers=false;expires=0;";
+    document.cookie = "displayBlockTeachers=false;expires="+date;
 }
